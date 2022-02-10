ebe9760bfcdab5625abd6e14d55c5d30920ec2a4
Fionn Malone <fmalone@google.com>
2022-02-10 20:00:38 +0000

    Column generation.

--- factorizations/kpoint_cholesky_factorization.py
+++ factorizations/kpoint_cholesky_factorization.py
@@ -22,6 +22,7 @@ def build_momentum_transfer_mapping(
 
     return momentum_transfer_map
 
+
 def generate_orbital_products(
         Q_index,
         kmf,
@@ -30,22 +31,39 @@ def generate_orbital_products(
     num_kpoints = len(kpoints)
     # For given Q generate [phi_{pk}^* phi_{q k-Q}](G)
     num_G = np.prod(kmf.cell.mesh)
-    num_pq = np.prod([C.shape[1] for C in kmf.mo_coeff])
-    rho_pq = np.zeros((num_kpoints, num_G, num_pq), dtype=np.complex128)
+    # TODO: Make AO
+    num_pq = [C.shape[1]*C.shape[1] for C in kmf.mo_coeff]
+    assert len(np.unique(num_pq)) == 1, "Number of MO per kp differs, please account for this."
+    rho_pq = np.zeros((num_kpoints, num_G, num_pq[0]), dtype=np.complex128)
     for k1 in range(num_kpoints):
         k2 = Qmap[Q_index, k1]
-        rho_pq[k1] = kmf.with_df.get_mo_pairs_G(
-                (kmf.mo_coeff[k1], kmf.mo_coeff[k2]),
-                (kpoints[k1],kpoints[k2]),
+        rho_pq[k1] = kmf.with_df.get_ao_pairs_G(
+                (kpoints[k1], kpoints[k2]),
                 kpoints[k2] - kpoints[k1],
                 compact=False)
         v_G = tools.get_coulG(
                 kmf.cell,
                 kpoints[k2] - kpoints[k1],
                 mesh=kmf.cell.mesh)
-        rho_pq[k1] *= v_G[:, None] * kmf.cell.vol / (num_G**2.0)
+        rho_pq[k1] *= np.sqrt(v_G[:, None] * kmf.cell.vol) / num_G
 
     return rho_pq
+
+def locate_max_residual(
+        residual,
+        mom_trans_indx,
+        momentum_map,
+        num_mo_per_kpoint):
+    assert len(np.unique(num_mo_per_kpoint)) == 1, "TODO: Fix this"
+    loc_max_res = np.argmax(residual)
+    num_pq = residual.shape[1]
+    max_k1_indx = loc_max_res // num_pq # row index
+    max_pq_indx = loc_max_res % num_pq # column index
+    max_k2_indx = momentum_map[mom_trans_indx, max_kpoint_indx]
+    max_res = residual[max_kpoint_index, max_pq_indx].real
+    # max_p_indx = max_pq_indx // num_mo_per_kpoint[k1]
+    # max_q_indx = max_pq_indx % num_mo_per_kpoint[k2]
+    return max_res, (max_k1_indx, max_k2_indx), (max_pq_indx)
 
 def build_eri_diagonal(
         mom_trans_indx,
@@ -56,29 +74,84 @@ def build_eri_diagonal(
     num_kpoints = rho_pq.shape[0]
     num_pq = rho_pq.shape[2]
     residual = np.zeros((num_kpoints, num_pq))
-    max_res = -1
     for k1 in range(num_kpoints):
         k2 = momentum_map[mom_trans_indx,k1]
         residual[k1] = np.einsum(
                         'GI,GI->I',
                         rho_pq[k1],
                         rho_pq[k1].conj()).real
-        max_pq_indx = np.argmax(residual[k1])
-        max_res_k = residual[k1, max_pq_indx].real
-        if max_res_k > max_res:
-            max_res = max_res_k
-            k1_max = k1
-            k2_max = k2
-            p_max = max_pq_indx // num_mo_per_kpoint[k1]
-            q_max = max_pq_indx % num_mo_per_kpoint[k2]
 
-    return residual, (k1_max, k2_max), (p_max, q_max)
+    return residual
+
+def generate_eri_column(
+        kmf,
+        rho_pq,
+        max_k3k4,
+        max_pq,
+        mom_trans_indx,
+        mom_trans_map,
+        kpoints):
+
+    num_G = np.prod(kmf.cell.mesh)
+    num_kpoints = len(kpoints)
+    num_pq = rho_pq.shape[2]
+    eri_column = np.zeros((num_kpoints, num_pq), dtype=np.complex128)
+    k3, k4 = max_k3k4
+    for ik1, k1 in enumerate(kpoints):
+        ik2 = mom_trans_map[mom_trans_indx, ik1]
+        # TODO only generate appropriate scalar.
+        rho_34 = kmf.with_df.get_ao_pairs_G(
+                (kpoints[k4], kpoints[k3]),
+                kpoints[ik2] - kpoints[ik1],
+                compact=False)[:,max_pq]
+        v_G = tools.get_coulG(
+                kmf.cell,
+                kpoints[ik2] - kpoints[ik1],
+                mesh=kmf.cell.mesh)
+        rho_34 *= np.sqrt(v_G * kmf.cell.vol) / num_G
+        eri_column[ik1] = np.einsum(
+                            'GI,G->I',
+                            rho_pq[ik1],
+                            rho_34.conj(),
+                            )
+    return eri_column
 
 def generate_kpoint_cholesky_factorization(
-        cell,
-        kpts,
-        hcore,
-        mo_coeff):
-
-    # 1. Build Diagonal V[(pk_p qk_q), (sk_s rk_r)] = (pk_p q k_q | rk_r sk_s)
-    pass
+        kmf,
+        kpoints,
+        threshold=1e-5,
+        max_chol_factor=10):
+    num_mo_per_kpoint = [C.shape[1] for C in kmf.mo_coeff]
+    mom_trans_map = build_momentum_transfer_mapping(kmf.cell, kpoints)
+    for mom_trans_indx, mom_trans in enumerate(kpoints):
+        max_num_chol = max_chol_factor * num_mo_per_kpoint[mom_trans_indx]
+        chol_vecs_iq = np.zeros((max_num_chol, num_pq), dtype=np.complex128)
+        # 1. Build (pq|G) for this Q
+        rho_pq = generate_orbital_products(
+                    mom_trans_indx,
+                    kmf,
+                    mom_transfer_map,
+                    kpoints)
+        num_pq = rho_pq.shape[2]
+        # 2. Build Diagonal V[(pk_p qk_q), (pk_q rk_p)] = (pk+Q q k | pk'-Q qk')
+        # Recall, we are factorizing matrix M[pq,sr] = \sum_X L_(pq,X) L_(sr,X)^* = (pq|rs)
+        eri_diag = build_eri_diagonal(
+                       mom_trans_indx,
+                       rho_pq,
+                       num_mo_per_kpoint,
+                       mom_transfer_map)
+        approx_eri_diag = np.zeros_like(eri_diag)
+        max_res, max_k1k2, max_pq = locate_max_residual(
+                                        eri_diag,
+                                        mom_trans_indx,
+                                        mom_trans_map,
+                                        num_mo_per_kpoint
+                                        )
+        chol_vecs_iq[0] = generate_eri_column(
+                             rho_pq,
+                             mom_trans_indx,
+                             mom_trans_map,
+                             max_k1k2,
+                             max_pq)
+        while max_residual > threshold:
+            pass
--- factorizations/kpoint_cholesky_factorization_test.py
+++ factorizations/kpoint_cholesky_factorization_test.py
@@ -6,7 +6,9 @@ from pyscf.pbc import scf, gto
 from kpoint_cholesky_factorization import (
         build_momentum_transfer_mapping,
         generate_orbital_products,
-        build_eri_diagonal
+        build_eri_diagonal,
+        generate_kpoint_cholesky_factorization,
+        generate_eri_column
         )
 
 def test_generate_momentum_transfer_mapping():
@@ -124,28 +126,151 @@ def test_build_eri_diagonal():
     cell.unit = 'B'
     cell.verbose = 4
     cell.build()
-    kpts = cell.make_kpts([2, 1, 1])
+    kpts = cell.make_kpts([3, 1, 1])
     a = cell.lattice_vectors() / (2*np.pi)
     kpoints = kpts
     num_kpoints = len(kpoints)
     mom_map = build_momentum_transfer_mapping(cell, kpts)
     kmf = scf.KRHF(cell, kpts, exxdiv=None)
     nao = cell.nao_nr()
-    kmf.mo_coeff = [np.random.random((nao,nao))] * len(kpoints)
-    mom_trans_indx = 1
+    kmf.mo_coeff = [np.eye(nao)] * len(kpoints)
     num_mo_per_kpoint = [nao] * len(kpoints)
+    num_kpoints = len(kpoints)
+    num_pq = nao*nao
+    from pyscf.pbc.df.fft_ao2mo import get_eri
+    for mom_trans_indx, mom_trans in enumerate(kpoints):
+        rho_pq = generate_orbital_products(
+                mom_trans_indx,
+                kmf,
+                mom_map,
+                kpoints)
+        eri_diag = build_eri_diagonal(
+                      mom_trans_indx,
+                      rho_pq,
+                      num_mo_per_kpoint,
+                      mom_map)
+        diag = np.zeros((num_kpoints,num_pq))
+        for kp1, kpt1 in enumerate(kpoints):
+            kp2 = mom_map[mom_trans_indx, kp1]
+            kpt_pqrs = [
+                        kpt1,
+                        kpoints[kp2],
+                        kpoints[kp2],
+                        kpt1,
+                        ]
+            eri_pqrs = get_eri(
+                    kmf.with_df,
+                    kpts=kpt_pqrs,
+                    compact=False)
+            eri_pqrs = eri_pqrs.reshape((nao,)*4).transpose((0,1,3,2)).reshape((num_pq,)*2)
+            diag[kp1] = eri_pqrs.diagonal().ravel()
+        assert np.max(diag) - np.max(eri_diag) < 1e-10
+        assert np.linalg.norm(diag-eri_diag) < 1e-10
+    assert eri_diag.shape == (num_kpoints, num_pq)
+
+def test_generate_cholesky_factorization():
+    cell = gto.Cell()
+    cell.atom = '''
+    C 0.000000000000   0.000000000000   0.000000000000
+    C 1.685068664391   1.685068664391   1.685068664391
+    '''
+    cell.basis = 'gth-szv'
+    cell.pseudo = 'gth-pade'
+    cell.a = '''
+    0.000000000, 3.370137329, 3.370137329
+    3.370137329, 0.000000000, 3.370137329
+    3.370137329, 3.370137329, 0.000000000'''
+    cell.unit = 'B'
+    cell.verbose = 4
+    cell.build()
+    kpts = cell.make_kpts([2, 1, 1])
+    a = cell.lattice_vectors() / (2*np.pi)
+    kpoints = kpts
+    num_kpoints = len(kpoints)
+    mom_map = build_momentum_transfer_mapping(cell, kpts)
+    kmf = scf.KRHF(cell, kpts, exxdiv=None)
+    # kmf.chkfile = 'diamond_222.chk'
+    from pyscf.scf.chkfile import load_scf
+    _, kmf_dict = load_scf('diamond_222.chk')
+    kmf.mo_coeff = kmf_dict['mo_coeff']
+    nao = cell.nao_nr()
+    # AO basis.
+    generate_kpoint_cholesky_factorization(kmf, kpoints)
+
+def test_generate_eri_column():
+    cell = gto.Cell()
+    cell.atom = '''
+    C 0.000000000000   0.000000000000   0.000000000000
+    C 1.685068664391   1.685068664391   1.685068664391
+    '''
+    cell.basis = 'gth-szv'
+    cell.pseudo = 'gth-pade'
+    cell.a = '''
+    0.000000000, 3.370137329, 3.370137329
+    3.370137329, 0.000000000, 3.370137329
+    3.370137329, 3.370137329, 0.000000000'''
+    cell.unit = 'B'
+    cell.verbose = 4
+    cell.build()
+    kpts = cell.make_kpts([3, 2, 1])
+    a = cell.lattice_vectors() / (2*np.pi)
+    kpoints = kpts
+    num_kpoints = len(kpoints)
+    mom_map = build_momentum_transfer_mapping(cell, kpts)
+    kmf = scf.KRHF(cell, kpts, exxdiv=None)
+    # kmf.chkfile = 'diamond_222.chk'
+    from pyscf.scf.chkfile import load_scf
+    nao = cell.nao_nr()
+    # AO basis.
+    kmf.mo_coeff = [np.eye(nao)] * len(kpoints)
+    num_mo_per_kpoint = [nao] * len(kpoints)
+    num_kpoints = len(kpoints)
+    num_pq = nao*nao
+    from pyscf.pbc.df.fft_ao2mo import get_eri
+    iQ = 1
+    # print(iQ)
+    ik4 = 2
+    ik3 = mom_map[iQ,ik4]
+    r = 6
+    s = 2
+    # max_rs = r*nao + s
+    # M[pq,rs] = (pq|sr)
+    max_rs = r*nao + s
+    # for mom_trans_indx, mom_trans in enumerate(kpoints):
     rho_pq = generate_orbital_products(
-            mom_trans_indx,
+            iQ,
             kmf,
             mom_map,
             kpoints)
-    num_kpoints = len(kpoints)
-    num_pq = nao*nao
-    residual, max_k1k2, max_pq = build_eri_diagonal(
-                                    mom_trans_indx,
-                                    rho_pq,
-                                    num_mo_per_kpoint,
-                                    mom_map)
-    assert residual.shape == (num_kpoints, num_pq)
-    assert len(max_k1k2) == 2
-    assert len(max_pq) == 2
+    eri_col = generate_eri_column(
+                    kmf,
+                    rho_pq,
+                    [ik3,ik4],
+                    max_rs,
+                    iQ,
+                    mom_map,
+                    kpoints)
+    diag = np.zeros_like(eri_col)
+    from pyscf.pbc.df.fft_ao2mo import get_ao_pairs_G, _iskconserv
+    from pyscf.pbc import tools
+    Q = kpoints[iQ]
+    eri_col_ref = np.zeros_like(eri_col)
+    for kp1, kpt1 in enumerate(kpoints):
+        kp2 = mom_map[iQ, kp1]
+        kpt_pqrs = [
+                    kpt1,
+                    kpoints[kp2],
+                    kpoints[ik3],
+                    kpoints[ik4],
+                    ]
+        eri_pqrs = get_eri(
+                kmf.with_df,
+                kpts=kpt_pqrs,
+                compact=False)
+        coulG = tools.get_coulG(
+                    kmf.cell,
+                    kpoints[kp2]-kpt1,
+                    mesh=kmf.cell.mesh)
+        eri_pqrs = eri_pqrs.reshape((nao,)*4).transpose((0,1,3,2)).reshape((num_pq,)*2)
+        eri_col_ref[kp1] = eri_pqrs[:,max_rs]
+        print("delta me: ", np.linalg.norm(eri_col[kp1]-eri_col_ref[kp1]))
