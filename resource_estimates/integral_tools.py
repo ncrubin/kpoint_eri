6f58befd39681738e24288210a18f9f0eea8e579
Fionn Malone <fionn.malone@gmail.com>
2022-04-19 07:06:09 +0000

    Add double factorized eri helper.

--- resource_estimates/integral_tools.py
+++ resource_estimates/integral_tools.py
@@ -95,8 +95,8 @@ def kpoint_cholesky_eris(
         nchol_pk = [chol_thresh] * num_kpoints
     for iq in range(num_kpoints):
         for ikp, iks in product(range(num_kpoints), repeat=2):
-            Lkp_minu_q = chol[iq][ikp,:,:,:nchol_pk[ikp]]
-            Lks_minu_q = chol[iq][iks,:,:,:nchol_pk[ikp]]
+            Lkp_minu_q = chol[iq][ikp,:,:,:nchol_pk[ikq]]
+            Lks_minu_q = chol[iq][iks,:,:,:nchol_pk[ikq]]
             ikq = momentum_map[iq, ikp]
             ikr = momentum_map[iq, iks]
             eri_pqrs = np.einsum(
@@ -138,7 +138,6 @@ class CholeskyHelper:
 
     def get_eri(self, ikpts):
         ikp, ikq, ikr, iks = ikpts
-        # print(ikp, ikq, ikr, iks)
         iq = self.mom_map_12[ikp, ikq]
         iq_ = self.mom_map_12[iks, ikr]
         assert iq == iq_
@@ -167,38 +166,153 @@ def cholesky_eri_helper(
     return eri_pqrs
 
 # 4. DF kpoint integrals
-# def kpoint_df_integrals(
-        # chol,
-        # kpoints,
-        # momentum_map,
-        # chol_thresh=None,
-        # df_thresh=0.0,
-        # ):
-    # nmo = sum(C.shape[1] for C in mo_coeff)
-    # eris = np.zeros((nmo,)*4, dtype=np.complex128)
-    # num_kpoints = momentum_map.shape[0]
-    # nmo_pk = [C.shape for C in mo_coeffs]
-    # offsets = np.cumsum(nmo_pk)
-    # nchol_pk = [L.shape[-1] for L in chol]
-    # if chol_thresh is not None:
-        # nchol_pk = [chol_thresh] * num_kpoints
-    # for iq in range(num_kpoints):
-        # for ikp, iks in product(range(num_kpoints), repeat=2):
-            # Lkp_minu_q = chol[iq,ikp,:,:,:nchol_pk[ikp]]
-            # Lks_minu_q = chol[iq,iks,:,:,:nchol_pk[ikp]]
-            # eri_pqrs = np.einsum(
-                    # 'pqn,srn->pqrs',
-                    # L_kp_minu_q,
-                    # L_ks_minu_q.conj(),
-                    # optimize=True)
-            # # Add DF
-            # P = slice(ikp*offsets[ikp], ikp*offsets[ikp] + nmo_pk[ikp])
-            # Q = slice(ikq*offsets[ikq], ikq*offsets[ikq] + nmo_pk[ikq])
-            # R = slice(ikr*offsets[ikr], ikr*offsets[ikr] + nmo_pk[ikr])
-            # S = slice(iks*offsets[iks], iks*offsets[iks] + nmo_pk[iks])
-            # eris[P,Q,R,S] = eri_pqrs
+class DFHelper:
 
-    # return eris
+    def __init__(
+            self,
+            chol,
+            mom_map,
+            kpoints,
+            nmo_pk,
+            chol_thresh=None,
+            df_thresh=0.0):
+        self.chol = chol
+        nchol_pk = [L.shape[-1] for L in chol]
+        self.offsets = np.cumsum(nmo_pk) - nmo_pk[0]
+        if chol_thresh is not None:
+            nchol_pk = [chol_thresh] * len(kpoints)
+        nchol = chol_thresh
+        self.nchol_pk = nchol_pk
+        self.kpoints = kpoints
+        self.mom_map = mom_map
+        self.df_thresh = df_thresh
+        self.nmo_pk = nmo_pk
+        k1k2_q = np.zeros_like(mom_map)
+        nk = len(kpoints)
+        nmo_tot = sum(nmo_pk)
+        for iq in range(nk):
+            for ik1 in range(nk):
+                for ik2 in range(nk):
+                    if mom_map[iq,ik1] == ik2:
+                        k1k2_q[ik1,ik2] = iq
+        self.mom_map_12 = k1k2_q
+        # store contiguously for ease of einsum
+        self.Us = np.zeros((nk, nchol, nmo_tot, nmo_tot), dtype=np.complex128)
+        self.Vs = np.zeros((nk, nchol, nmo_tot, nmo_tot), dtype=np.complex128)
+        self.lambda_A = np.zeros((nk, nchol, nmo_tot), dtype=np.complex128)
+        self.lambda_B = np.zeros((nk, nchol, nmo_tot), dtype=np.complex128)
+        # Build DF factors
+        for iq in range(nk):
+            # print(iq)
+            for nc in range(nchol):
+                # print(nc)
+                A, B = self.build_AB(iq, nc, self.mom_map)
+                eigs, eigv = self.get_df_factor(A, self.df_thresh)
+                self.lambda_A[iq, nc] = eigs
+                self.Us[iq, nc] = eigv
+                eigs, eigv = self.get_df_factor(B, self.df_thresh)
+                self.lambda_B[iq, nc] = eigs
+                self.Vs[iq, nc] = eigv
+
+    def get_eri(self, ikpts):
+        ikp, ikq, ikr, iks = ikpts
+        iq = self.mom_map_12[ikp, ikq]
+        iq_ = self.mom_map_12[iks, ikr]
+        assert iq == iq_
+        # (p kp q q kp - Q | r ks - Q s ks)
+        # (pq|rs) = A^2 + B^2
+        #         = U[n,pq,t]* e^2 U[n,pq, t]
+        P = slice(ikp*self.offsets[ikp], ikp*self.offsets[ikp] + self.nmo_pk[ikp])
+        Q = slice(ikq*self.offsets[ikq], ikq*self.offsets[ikq] + self.nmo_pk[ikq])
+        R = slice(ikr*self.offsets[ikr], ikr*self.offsets[ikr] + self.nmo_pk[ikr])
+        S = slice(iks*self.offsets[iks], iks*self.offsets[iks] + self.nmo_pk[iks])
+        # print(iq)
+        # U = self.Us[iq]
+        # eri_A = np.einsum(
+                # 'nPt,nt,nQt,nRs,ns,nSs->PQRS',
+                # U,
+                # self.lambda_A[iq],
+                # U.conj(),
+                # U,
+                # self.lambda_A[iq],
+                # U.conj(),
+                # optimize=True)
+        # V = self.Vs[iq]
+        # eri_B = np.einsum(
+                # 'nPt,nt,nQt,nRs,ns,nSs->PQRS',
+                # V,
+                # self.lambda_B[iq],
+                # V.conj(),
+                # V,
+                # self.lambda_B[iq],
+                # V.conj(),
+                # optimize=True)
+        # eris = eri_A + eri_B
+        # return eris[P,Q,R,S] * len(self.kpoints)
+        UP = self.Us[iq,:,P,:].copy()
+        UQ = self.Us[iq,:,Q,:].copy()
+        UR = self.Us[iq,:,R,:].copy()
+        US = self.Us[iq,:,S,:].copy()
+        eri_A = np.einsum(
+                'nPt,nt,nQt,nRr,nr,nSr->PQRS',
+                UP,
+                self.lambda_A[iq],
+                UQ.conj(),
+                UR,
+                self.lambda_A[iq],
+                US.conj(),
+                optimize=True)
+        VP = self.Vs[iq,:,P,:].copy()
+        VQ = self.Vs[iq,:,Q,:].copy()
+        VR = self.Vs[iq,:,R,:].copy()
+        VS = self.Vs[iq,:,S,:].copy()
+        eri_B = np.einsum(
+                'nPt,nt,nQt,nRr,nr,nSr->PQRS',
+                VP,
+                self.lambda_B[iq],
+                VQ.conj(),
+                VR,
+                self.lambda_B[iq],
+                VS.conj(),
+                optimize=True)
+
+        eri = eri_A + eri_B
+        return eri * len(self.kpoints)
+
+    @staticmethod
+    def get_df_factor(mat, thresh):
+        eigs, eigv = np.linalg.eigh(mat)
+        # normSC = np.sum(np.abs(eigs))
+        # ix = np.argsort(np.abs(eigs))[::-1]
+        # eigs = eigs[ix]
+        # eigv = eigv[:,ix]
+        # truncation = normSC * np.abs(eigs)
+        # to_zero = truncation < thresh
+        # eigs[to_zero] = 0.0
+        # eigv[:,to_zero] = 0.0
+        return eigs, eigv
+
+    def build_AB(self, Q_index, chol_indx, momentum_map):
+        # print(Q_index, chol_indx)
+        LQn = self.chol[Q_index][:, :, :, chol_indx]
+        nmo = LQn.shape[1]
+        assert len(LQn.shape) == 3
+        num_kpoints = LQn.shape[0]
+        M = np.zeros((nmo*num_kpoints, nmo*num_kpoints), dtype=np.complex128)
+        A = np.zeros((nmo*num_kpoints, nmo*num_kpoints), dtype=np.complex128)
+        B = np.zeros((nmo*num_kpoints, nmo*num_kpoints), dtype=np.complex128)
+        for kp in range(num_kpoints):
+            kq = momentum_map[Q_index, kp]
+            for p, q in product(range(nmo), repeat=2):
+                P = int(kp * nmo + p)
+                Q = int(kq * nmo + q)
+                M[P,Q] += LQn[kp, p, q]
+                # M[Q,P] += LQn[kp, p, q].conj()
+        A = 0.5 * (M + M.conj().T)
+        B = 0.5*1j * (M - M.conj().T)
+        assert np.linalg.norm(A-A.conj().T) < 1e-12
+        assert np.linalg.norm(B-B.conj().T) < 1e-12
+        return A, B
 
 # 5. THC supercell integrals
 def thc_eris(
--- resource_estimates/test_integral_tools.py
+++ resource_estimates/test_integral_tools.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyscf.pbc.df import fft, fft_ao2mo
 
 from integral_tools import (
         supercell_eris,
@@ -53,9 +54,117 @@ def test_kpoint_cholesky_integrals():
             nmo_pk,
             )
 
+def test_df_integrals():
+    chkfile = 'data/scf_nk2.chk'
+    cell, kmf = init_from_chkfile(chkfile)
+    hamil_chol = read_qmcpack_cholesky_kpoint('data/chol_nk2.h5')
+    nmo_per_kp = hamil_chol['nmo_pk']
+    nchol_per_kp = hamil_chol['nchol_pk']
+    nk = len(nmo_per_kp)
+    nmo_k1k2 = []
+    max_nmo = np.max(nmo_per_kp)
+    chol = hamil_chol['chol']
+    kpoints = hamil_chol['kpoints']
+    nchol_per_kp = hamil_chol['nchol_pk']
+    qk_to_k2 = hamil_chol['qk_k2']
+    from integral_tools import (
+            DFHelper,
+            CholeskyHelper,
+            ERIHelper)
+    df = fft.FFTDF(cell, kpts=kpoints)
+    eh = ERIHelper(
+            df,
+            kmf.mo_coeff,
+            kpoints)
+    ikpts = [0, 1, 1 ,0]
+    eri_ref = eh.get_eri(ikpts)
+    ch = CholeskyHelper(
+            chol,
+            qk_to_k2,
+            kpoints,
+            chol_thresh=264,
+            )
+    eri_chol = ch.get_eri(ikpts)
+    print(np.max(np.abs(eri_ref-eri_chol)))
+    dfh = DFHelper(
+            chol,
+            qk_to_k2,
+            kpoints,
+            nmo_per_kp,
+            chol_thresh=264,
+            df_thresh=0.0)
+    eri_df = dfh.get_eri(ikpts)
+    # print(eri_ref[0,0,0,0], eri_df[0,0,0,0])
+    print(np.max(np.abs(eri_ref-eri_df)))
+
 def test_thc_integrals():
     hamil_thc = read_qmcpack_thc('data/thc_nk2_cthc12.h5')
     eris = thc_eris(
             hamil_thc['orbs_pu'],
             hamil_thc['Muv']
             )
+
+# def test_random_df():
+    # no = 4
+    # eris = np.random.normal(scale=0.01, size=(no,)*4)
+    # eris = eris + eris.transpose((2,3,0,1))
+    # eris = eris + eris.transpose((3,2,1,0))
+    # eris = eris + eris.transpose((1,0,2,3))
+    # eris = eris + eris.transpose((0,1,3,2))
+    # eris = eris.reshape((no*no,no*no))
+    # eris = np.dot(eris,eris.T)
+    # chol = modified_cholesky(eris, tol=1e-12)
+    # chol = chol.reshape((-1, no, no))
+    # print(np.linalg.norm(
+        # eris.reshape((no,)*4)- np.einsum('npq,nrs->pqrs', chol, chol, optimize=True)
+        # ))
+    # nchol = chol.shape[0]
+    # Us = np.zeros((nchol, no, no))
+    # ls = np.zeros((nchol, no))
+    # for i in range(nchol):
+        # print(np.linalg.norm(chol[i] - chol[i].T))
+        # eigs, eigv = np.linalg.eigh(chol[i])
+        # Us[i] = eigv
+        # ls[i] = eigs
+
+    # eri_df = np.einsum(
+            # 'nPt,nt,nQt,nRs,ns,nSs->PQRS',
+            # Us, ls, Us.conj(), Us, ls, Us.conj(),
+            # optimize=True)
+    # print(np.linalg.norm(
+        # eris.reshape((no,)*4) - eri_df)
+        # )
+
+def test_random_df_cmplx():
+    no = 4
+    from ipie.utils.testing import generate_hamiltonian
+    h1e, chol, enuc, eris = generate_hamiltonian(no, (2,2), cplx=4, sym=4, tol=1e-12)
+    chol = chol.reshape((-1, no, no))
+    print("delta: ", np.linalg.norm(
+        eris.reshape((no,)*4)- np.einsum('npq,nsr->pqrs', chol, chol.conj(), optimize=True)
+        ))
+    nchol = chol.shape[0]
+    Us = np.zeros((nchol, no, no), dtype=np.complex128)
+    ls = np.zeros((nchol, no), dtype=np.complex128)
+    Vs = np.zeros((nchol, no, no), dtype=np.complex128)
+    ts = np.zeros((nchol, no), dtype=np.complex128)
+    for i in range(nchol):
+        # print(np.linalg.norm(chol[i] - chol[i].conj().T))
+        eigs, eigv = np.linalg.eigh(0.5*(chol[i]+chol[i].conj().T))
+        Us[i] = eigv
+        ls[i] = eigs
+        eigs, eigv = np.linalg.eigh(0.5*1j*(chol[i]-chol[i].conj().T))
+        Vs[i] = eigv
+        ts[i] = eigs
+
+    eri_df_A = np.einsum(
+            'nPt,nt,nQt,nRs,ns,nSs->PQRS',
+            Us, ls, Us.conj(), Us, ls, Us.conj(),
+            optimize=True)
+    eri_df_B = np.einsum(
+            'nPt,nt,nQt,nRs,ns,nSs->PQRS',
+            Vs, ts, Vs.conj(), Vs, ts, Vs.conj(),
+            optimize=True)
+    print("delta 2: ", np.linalg.norm(
+        eris - (eri_df_A+eri_df_B))
+        )
