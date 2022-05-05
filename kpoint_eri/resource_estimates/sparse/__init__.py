4c03ff4b948ec58166a2415ca135c902ea3719e4
Fionn Malone <fionn.malone@gmail.com>
2022-05-05 18:27:52 +0000

    Update imports.

--- kpoint_eri/resource_estimates/sparse/__init__.py
+++ kpoint_eri/resource_estimates/sparse/__init__.py
@@ -1,1 +1,5 @@
-from .sparse_eris import count_number_of_non_zero_elements
+from .sparse_eris import (
+        count_number_of_non_zero_elements,
+        build_sparse_eris,
+        build_eris_kpt
+        )
