fixes bug in 1-electron lambda calculation

kpoint_eri/resource_estimates/df/compute_lambda_df.py
@@ -87,7 +87,7 @@ def compute_lambda_ncr(hcore, df_obj: DFABKpointIntegrals):
             eri_kkqq_pqrs = df_obj.get_eri_exact([kidx, kidx, qidx, qidx])  
             h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
 
-        one_body_mat[kidx] = hcore[kidx] - 0.5 * h1_neg + 0.5 * h1_pos
+        one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + 0.5 * h1_pos
         one_eigs, _ = np.linalg.eigh(one_body_mat[kidx])
         lambda_one_body += np.sum(np.abs(one_eigs))
     

kpoint_eri/resource_estimates/df/ncr_integral_helper_df_test.py
@@ -28,7 +28,7 @@ def test_ab_decomp():
     pp_name = cell.pseudo
 
     kmesh = [1, 1, 3]
-    kpts = cell.make_kpts(kmesh)
+    kpts = cell.make_kpts(kmesh, scaled_center=[0.2, 0.3, 0.5])
 
     nkpts = len(kpts)
     mf = scf.KRHF(cell, kpts).rs_density_fit()
@@ -46,7 +46,7 @@ def test_ab_decomp():
 
     Luv = cholesky_from_df_ints(mymp)  # [kpt, kpt, naux, nao, nao]
     dfk_inst = DFABKpointIntegrals(Luv.copy(), mf)
-    df_factors = dfk_inst.double_factorize(1.0E-13)
+    df_factors = dfk_inst.double_factorize(1.0E-15)
 
     test_a_mats = np.empty((dfk_inst.nk, dfk_inst.naux), dtype=object)
     test_b_mats = np.empty((dfk_inst.nk, dfk_inst.naux), dtype=object)

kpoint_eri/resource_estimates/sf/compute_lambda_sf.py
@@ -82,7 +82,7 @@ def compute_lambda_ncr(hcore, sf_obj: NCRSingleFactorizationHelper):
             eri_kkqq_pqrs = sf_obj.get_eri([kidx, kidx, qidx, qidx])  
             h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
 
-        one_body_mat[kidx] = hcore[kidx] - 0.5 * h1_neg + 0.5 * h1_pos
+        one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + 0.5 * h1_pos
         one_eigs, _ = np.linalg.eigh(one_body_mat[kidx])
         lambda_one_body += np.sum(np.abs(one_eigs))
 