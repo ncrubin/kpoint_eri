85e87f4ccb2baf374466ac57e920debdf6fdfc7a
Nicholas Rubin <rubinnc0@gmail.com>
2022-10-03 22:50:50 -0700

    WIP alpha-beta df kpoint

--- kpoint_eri/resource_estimates/df/ncr_integral_helper_df.py
+++ kpoint_eri/resource_estimates/df/ncr_integral_helper_df.py
@@ -193,3 +193,218 @@ class DFABKpointIntegrals:
         """
         ikp, ikq, ikr, iks = ikpts
         return np.einsum('npq,nsr->pqrs', self.chol[ikp, ikq], self.chol[iks, ikr].conj(), optimize=True)
+
+class DFAlphaBetaKpointIntegrals:
+    def __init__(self, cholesky_factor: np.ndarray, kmf: scf.HF):
+        """
+        Initialize a ERI object for CCSD from Cholesky factors and a
+        pyscf mean-field object
+
+        We need to form the alpha and beta objects which are indexed by Cholesky index n, 
+        momentum modes, k, k', Q, and an even/odd index. This is accomplished by constructing 
+        rho[Q, k, n, nao, nao] by 
+        reshaping the cholesky object.  We don't form the matrix  
+
+        :param cholesky_factor: Cholesky factor tensor that is [nkpts, nkpts, naux, nao, nao]
+        :param kmf: pyscf k-object.  Currently only used to obtain the number of k-points.
+                    must have an attribute kpts which len(self.kmf.kpts) returns number of 
+                    kpts.
+        """
+        self.chol = cholesky_factor
+        self.kmf = kmf 
+        self.nk = len(self.kmf.kpts)
+        self.nao = self.kmf.cell.nao
+        self.naux = self.chol[0, 0].shape[0]
+        kpts = self.kmf.kpts
+        cell = self.kmf.cell
+        k_transfer_map = build_momentum_transfer_mapping(self.kmf.cell, self.kmf.kpts)
+        self.k_transfer_map = k_transfer_map
+
+        # set up for later when we construct DF
+        self.df_factors = None
+        self.a_mats = None
+        self.b_mats = None
+
+        # slice setup
+        nmo =self.nao
+        self.k_slice = slice(0, nmo, 1)
+        self.kmq_slice = slice(1 * nmo, (1 + 1) * nmo, 1)
+        self.kp_slice = slice(2 * nmo, (2 + 1) * nmo, 1)
+        self.kpmq_slice = slice(3 * nmo, (3 + 1) * nmo, 1)
+
+
+        
+    def build_alpha_beta_from_chol(self, kidx, kpidx, qidx):
+        """
+        Construct alpha and beta matrices given three momentum mode indices
+
+        Every alpha and beta are defined over a one-particle Hilbert space of size 4 * nmo
+        
+        the 4 is from the fixed momentum modes (k, k-Q, k', k'-Q) below is a diagram of how we 
+        store the matrix
+
+              k  | k-Q | k' | k' - Q
+            ------------------------
+        k   |    |     |    |      |
+        ----------------------------
+        k-Q |    |     |    |      | 
+        ----------------------------
+        k'  |    |     |    |      |
+        ----------------------------
+        k'-Q|    |     |    |      |
+        ----------------------------
+
+        a particular slice can be accessed by 
+        k-slice = [0 * nmo: (0 + 1) * nmo:1]
+        k-Q slice = [1 * nmo:(1 + 1) * nmo:1]
+        k'-slice =  [2 * nmo:(2 + 1) * nmo:1]
+        k'-Q-slice =  [3 * nmo:(3 + 1) * nmo:1]
+
+        :param kidx: k-momentum index
+        :param kpidx: k'-momentum index
+        :param qidx: index for momentum mode Q.  
+        :returns: Tuple(np.ndarray, np.ndarray) where each array is 
+                  [naux, 4 * nmo, 4 * nmo] set of matrices.
+        """
+        nmo = self.nao # number of gaussians used
+        naux = self.naux
+
+        # now form A and B
+        # First set up matrices to store. We will loop over Q index and construct
+        # entire set of matrices index by n-aux.
+        rho_k = np.zeros((naux, 4 * nmo, 4 * nmo), dtype=np.complex128)
+        rho_kp = np.zeros((naux, 4 * nmo, 4 * nmo), dtype=np.complex128)
+
+        k_minus_q_idx = self.k_transfer_map[qidx, kidx]
+        kp_minus_q_idx = self.k_transfer_map[qidx, kpidx]
+
+        rho_k[:, self.k_slice, self.kmq_slice] = self.chol[kidx, k_minus_q_idx][:, :, :]
+        rho_kp[:, self.kp_slice, self.kpmq_slice] = self.chol[kpidx, kp_minus_q_idx][:, :, :]
+            
+        A = 0.5  * (rho_k + rho_kp.transpose((0,2,1)).conj())
+        Adag = 0.5 * (rho_k.transpose((0,2,1)).conj() + rho_kp)
+        B = 0.5j * (rho_k - rho_kp.transpose((0,2,1)).conj())
+        Bdag = 0.5j * (rho_kp - rho_k.transpose((0,2,1)).conj())
+
+        assert np.allclose(rho_k, A + -1j * B)          
+        assert np.allclose(rho_kp, Adag + -1j * Bdag)
+
+        alpha_p = 0.5 * (A + Adag)
+        alpha_m = 0.5 * (A - Adag)
+        beta_p = 0.5 * (B + Bdag)
+        beta_m = 0.5 * (B - Bdag)
+        return alpha_p, alpha_m, beta_p, beta_m
+    
+    def build_chol_part_from_alpha_beta(self, 
+                                        kidx: int, 
+                                        kpidx: int, 
+                                        qidx: int,
+                                        alpha_p: np.ndarray, 
+                                        alpha_m: np.ndarray,
+                                        beta_p: np.ndarray,
+                                        beta_m: np.ndarray) -> np.ndarray:
+        """
+        Construct rho_{n, k, Q} which is equal to the cholesky factor by summing
+        together via the following relationships
+
+        A_{n, k, k', Q} = alpha_p + alpha_m
+        A_{n, k, k', Q}^dag = alpha_p - alpha_m
+        B_{n, k, k', Q} = beta_p + beta_m
+        B_{n, k, k', Q}^dag = beta_p - beta_m
+
+        and 
+
+        rho_k = A + 1j * B
+        rho_kp = Adag + -1j * Bdag
+
+        :param kidx: k-momentum index
+        :param kpidx: k'-momentum index
+        :param qidx: Q-momentum index
+        :param alpha_p: [naux, 4 * nmo, 4 * nmo]
+        :param alpha_m: [naux, 4 * nmo, 4 * nmo]
+        :param beta_p: [naux, 4 * nmo, 4 * nmo]
+        :param beta_m: [naux, 4 * nmo, 4 * nmo]
+        :returns: cholesky factors 3-tensors (k, k-Q)[naux, nao, nao], (kp, kp-Q)[naux, nao, nao]
+        """
+        A = alpha_p + alpha_m
+        Adag = alpha_p - alpha_m
+        B = beta_p + beta_m
+        Bdag = beta_p - beta_m
+        rho_k = A + -1j * B
+        rho_kp = Adag + -1j * Bdag
+        return rho_k[:, self.k_slice, self.kmq_slice], rho_kp[:, self.kp_slice, self.kpmq_slice]
+
+
+    def double_factorize(self, thresh=None):
+        """
+        construct a double factorization of the Hamiltonian check's object if 
+        we have already constructed the DF then returns
+
+        :returns dict: Dict with keys U, lambda_U, V, lambda_V
+                       where U and V are matrices size nk * nmo x Z
+                       lambda_U and lambda_v are size Z vectors.  
+        """
+        if thresh is None:
+            thresh = 1.0E-13
+        if self.df_factors is not None:
+            return self.df_factors
+
+        a_mats = np.empty((self.nk,), dtype=object)
+        b_mats = np.empty((self.nk,), dtype=object)
+        u_basis = np.empty((self.nk, self.naux), dtype=object)
+        u_lambda = np.empty((self.nk, self.naux), dtype=object)
+        v_basis = np.empty((self.nk, self.naux), dtype=object)
+        v_lambda = np.empty((self.nk, self.naux), dtype=object)
+
+        for iq in range(self.nk):
+            A, B = self.build_AB_from_chol(iq)
+            a_mats[iq] = np.zeros((self.naux, self.nao * self.nk, self.nao * self.nk), dtype=np.complex128)
+            b_mats[iq] = np.zeros((self.naux, self.nao * self.nk, self.nao * self.nk), dtype=np.complex128)
+            for nc in range(self.naux):
+                # diagonalize A-mat at thresh 
+                A_eigs, A_eigv = get_df_factor(A[nc], thresh)
+                u_basis[iq, nc] = A_eigv[:, :]
+                u_lambda[iq, nc] = A_eigs[:]
+                a_mats[iq][nc] = A_eigv @ np.diag(A_eigs) @ A_eigv.conj().T
+
+                # diagonalize B-mat at thresh
+                B_eigs, B_eigv = get_df_factor(B[nc], thresh)
+                v_basis[iq, nc] = B_eigv[:, :]
+                v_lambda[iq, nc] = B_eigs[:]
+                b_mats[iq][nc] = B_eigv @ np.diag(B_eigs) @ B_eigv.conj().T
+
+        self.df_factors  = {'U': u_basis, 'lambda_U': u_lambda,
+                            'V': v_basis, 'lambda_V': v_lambda}
+        self.a_mats = a_mats
+        self.b_mats = b_mats
+        return self.df_factors
+
+    def get_eri(self, ikpts, a_mats=None, b_mats=None):
+        """
+        Construct (pkp qkq| rkr sks) via A and B tensors that have already been constructed
+
+        :param ikpts: list of four integers representing the index of the kpoint in self.kmf.kpts
+        """
+        if (self.a_mats is None or self.b_mats is None) and (a_mats is None and b_mats is None):
+            raise ValueError("No DF factorization has occured yet. Rerun by calling inst.double_factorize()")
+        
+        if a_mats is None:
+            a_mats = self.a_mats
+        if b_mats is None:
+            b_mats = self.b_mats 
+
+        ikp, ikq, ikr, iks = ikpts
+
+        # build Cholesky vector from truncated A and B
+        Luv = self.build_chol_from_AB(a_mats, b_mats)  
+        return np.einsum('npq,nsr->pqrs', Luv[ikp, ikq], Luv[iks, ikr].conj(), optimize=True)
+    
+    def get_eri_exact(self, ikpts):
+        """
+        Construct (pkp qkq| rkr sks) exactly from Cholesky vector.  This is for constructing the J and K like terms
+        needed for the one-body component lambda
+
+        :param ikpts: list of four integers representing the index of the kpoint in self.kmf.kpts
+        """
+        ikp, ikq, ikr, iks = ikpts
+        return np.einsum('npq,nsr->pqrs', self.chol[ikp, ikq], self.chol[iks, ikr].conj(), optimize=True)
--- kpoint_eri/resource_estimates/df/ncr_integral_helper_df_test.py
+++ kpoint_eri/resource_estimates/df/ncr_integral_helper_df_test.py
@@ -2,9 +2,9 @@ import itertools
 
 import numpy as np
 
-from pyscf.pbc import gto, scf, mp, cc
+from pyscf.pbc import gto, scf, mp, cc, tools
 
-from kpoint_eri.resource_estimates.df.ncr_integral_helper_df import DFABKpointIntegrals
+from kpoint_eri.resource_estimates.df.ncr_integral_helper_df import DFABKpointIntegrals, DFAlphaBetaKpointIntegrals
 
 
 def test_ab_decomp():
@@ -83,3 +83,61 @@ def test_ab_decomp():
     exact_cc.kernel()
     assert np.isclose(approx_cc.e_corr, exact_cc.e_corr)
     assert np.isclose(exact_emp2, emp2)
+
+def test_alphabeta_decomp():
+    cell = gto.Cell()
+    cell.atom='''
+    C 0.000000000000   0.000000000000   0.000000000000
+    C 1.685068664391   1.685068664391   1.685068664391
+    '''
+    cell.basis = 'gth-szv'
+    cell.pseudo = 'gth-hf-rev'
+    cell.a = '''
+    0.000000000, 3.370137329, 3.370137329
+    3.370137329, 0.000000000, 3.370137329
+    3.370137329, 3.370137329, 0.000000000'''
+    cell.unit = 'B'
+    cell.verbose = 4
+    cell.build()
+
+    name_prefix = ''
+    basname = cell.basis
+    pp_name = cell.pseudo
+
+    kmesh = [1, 1, 3]
+    kpts = cell.make_kpts(kmesh, scaled_center=[0.2, 0.3, 0.5])
+
+    nkpts = len(kpts)
+    mf = scf.KRHF(cell, kpts).rs_density_fit()
+    mf.chkfile = 'ncr_test_C_density_fitints.chk'
+    mf.init_guess = 'chkfile'
+    mf.with_df._cderi_to_save = 'ncr_test_C_density_fitints_gdf.h5'
+    mf.init_guess = 'chkfile'
+    mf.kernel()
+
+    from kpoint_eri.factorizations.pyscf_chol_from_df import cholesky_from_df_ints
+    mymp = mp.KMP2(mf)
+    nmo = mymp.nmo
+    nocc = mymp.nocc
+    nvir = nmo - nocc
+
+    Luv = cholesky_from_df_ints(mymp)  # [kpt, kpt, naux, nao, nao]
+    dfk_inst = DFAlphaBetaKpointIntegrals(Luv.copy(), mf)
+
+    kconserv = tools.get_kconserv(cell, kpts)
+    nkpts = dfk_inst.nk
+    # recall (k, k-q|k'-q, k')
+    for kidx in range(nkpts):
+        for kpidx in range(nkpts):
+            for qidx in range(nkpts):                 
+                kmq_idx = dfk_inst.k_transfer_map[qidx, kidx]
+                kpmq_idx = dfk_inst.k_transfer_map[qidx, kpidx]
+                alpha_p, alpha_m, beta_p, beta_m = \
+                    dfk_inst.build_alpha_beta_from_chol(kidx, kpidx, qidx)
+                assert np.allclose(alpha_p, alpha_p.transpose((0,2,1)).conj())
+                assert np.allclose(alpha_m, -alpha_m.transpose((0,2,1)).conj())
+                assert np.allclose(beta_p, beta_p.transpose((0,2,1)).conj())
+                assert np.allclose(beta_m, -beta_m.transpose((0,2,1)).conj())
+
+
+test_alphabeta_decomp()
