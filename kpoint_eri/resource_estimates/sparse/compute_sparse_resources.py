51aee14fbb04d0bdd5438611cf0232ef25251807
Nicholas Rubin <rubinnc0@gmail.com>
2022-10-17 12:16:54 -0700

    remove main run

--- kpoint_eri/resource_estimates/sparse/compute_sparse_resources.py
+++ kpoint_eri/resource_estimates/sparse/compute_sparse_resources.py
@@ -110,22 +110,3 @@ def cost_sparse(n: int, Nk: int, lam: float, d: int, dE: float, chi: int,
     ancilla_cost = int(ancilla_cost)
 
     return step_cost, total_cost, ancilla_cost
-
-if __name__ == "__main__":
-    nLi = 152
-    lam_Li = 1547.3
-    dLi = 440501
-    dE = 0.001
-    chi = 10
-    nK = 8
-
-    # n: int, Nk: int, lam: float, d: int, dE: float, chi: int, stps: int) -> Tuple[int, int, int]
-    stps2 = cost_sparse(nLi, nK, lam_Li, dLi, dE, chi, 20_000)
-    toff_per_step, total_toff, total_ancilla_cost = cost_sparse(nLi, nK, lam_Li, dLi, dE, chi, stps2[0])
-    print(toff_per_step)
-    print(total_toff)
-    print(total_ancilla_cost)
-    assert toff_per_step == 18914
-    assert total_toff == 45_970_363_516
-    assert total_ancilla_cost == 4294
-
