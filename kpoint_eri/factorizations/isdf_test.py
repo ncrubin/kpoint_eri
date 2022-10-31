eb037db53f9e9ba5fda11ebaeeb0e68a66bee3c2
Fionn Malone <fmalone@google.com>
2022-10-31 05:01:01 +0000

    Update unit test.

--- kpoint_eri/factorizations/isdf_test.py
+++ kpoint_eri/factorizations/isdf_test.py
@@ -2,7 +2,7 @@ import h5py
 import numpy as np
 import pytest
 
-from pyscf.pbc import gto, scf
+from pyscf.pbc import gto, scf, tools
 from pyscf.pbc.dft import numint
 
 from kpoint_eri.factorizations.kmeans import KMeansCVT
@@ -21,7 +21,7 @@ def test_supercell_isdf_gamma():
     3.370137329, 0.000000000, 3.370137329
     3.370137329, 3.370137329, 0.000000000"""
     cell.unit = "B"
-    cell.verbose = 4
+    cell.verbose = 0
     cell.build()
     # kpts = cell.make_kpts(kmesh, scaled_center=[0.2, 0.3, 0.5])
 
@@ -31,6 +31,7 @@ def test_supercell_isdf_gamma():
     mf.kernel()
 
     from pyscf.pbc.dft import gen_grid
+
     grid_inst = gen_grid.UniformGrids(cell)
     grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
     orbitals = numint.eval_ao(cell, grid_points)
@@ -38,43 +39,52 @@ def test_supercell_isdf_gamma():
     num_mo = mf.mo_coeff.shape[1]
     num_interp_points = 10 * num_mo
     nocc = cell.nelec[0]
-    density = np.einsum("Ri,Ri->R", orbitals_mo[:,:nocc],
-                        orbitals_mo[:,:nocc].conj(), optimize=True)
+    density = np.einsum(
+        "Ri,Ri->R", orbitals_mo[:, :nocc], orbitals_mo[:, :nocc].conj(), optimize=True
+    )
     assert np.einsum("R,R->", density, grid_inst.weights) == pytest.approx(nocc)
     with h5py.File(mf.chkfile, "r+") as fh5:
         try:
             interp_indx = fh5["interp_indx"][:]
         except KeyError:
             kmeans = KMeansCVT(grid_points, max_iteration=500)
-            interp_indx = kmeans.find_interpolating_points(num_interp_points,
-                                                           density)
+            interp_indx = kmeans.find_interpolating_points(num_interp_points, density)
             fh5["interp_indx"] = interp_indx
 
     from kpoint_eri.factorizations.isdf import supercell_isdf
-    chi, zeta, Theta = supercell_isdf(mf.with_df, interp_indx,
-                                      orbitals=orbitals_mo,
-                                      grid_points=grid_points)
+
+    chi, zeta, Theta = supercell_isdf(
+        mf.with_df, interp_indx, orbitals=orbitals_mo, grid_points=grid_points
+    )
     assert Theta.shape == (len(grid_points), num_interp_points)
     # Check overlap
     ovlp_ao = mf.get_ovlp()
     # should be identity matrix
-    ovlp_mo = np.einsum("pi,pq,qj->ij", mf.mo_coeff.conj(), ovlp_ao,
-                        mf.mo_coeff, optimize=True)
+    ovlp_mo = np.einsum(
+        "pi,pq,qj->ij", mf.mo_coeff.conj(), ovlp_ao, mf.mo_coeff, optimize=True
+    )
     identity = np.eye(num_mo)
     assert np.allclose(ovlp_mo, identity)
     ovlp_mu = np.einsum("Rm,R->m", Theta, grid_inst.weights, optimize=True)
     orbitals_mo_interp = orbitals_mo[interp_indx]
-    ovlp_isdf = np.einsum("mi,mj,m->ij", orbitals_mo_interp.conj(), orbitals_mo_interp,
-                          ovlp_mu, optimize=True)
+    ovlp_isdf = np.einsum(
+        "mi,mj,m->ij",
+        orbitals_mo_interp.conj(),
+        orbitals_mo_interp,
+        ovlp_mu,
+        optimize=True,
+    )
     assert np.allclose(ovlp_mo, ovlp_isdf)
     # Check ERIs.
     eri_ref = mf.with_df.ao2mo(mf.mo_coeff)
     from pyscf import ao2mo
+
     eri_ref = ao2mo.restore(1, eri_ref, num_mo)
     # THC eris
     Lijn = np.einsum("mi,mj,mn->ijn", chi.conj(), chi, zeta, optimize=True)
     eri_thc = np.einsum("ijn,nk,nl->ijkl", Lijn, chi.conj(), chi, optimize=True)
     assert np.allclose(eri_thc, eri_ref)
+
 
 def test_supercell_isdf_complex():
     cell = gto.Cell()
@@ -89,7 +99,7 @@ def test_supercell_isdf_complex():
     3.370137329, 0.000000000, 3.370137329
     3.370137329, 3.370137329, 0.000000000"""
     cell.unit = "B"
-    cell.verbose = 4
+    cell.verbose = 0
     cell.build()
     # kpts = cell.make_kpts(kmesh, scaled_center=[0.2, 0.3, 0.5])
 
@@ -100,6 +110,7 @@ def test_supercell_isdf_complex():
     assert np.max(np.abs(mf.mo_coeff.imag)) > 1e-2
 
     from pyscf.pbc.dft import gen_grid
+
     grid_inst = gen_grid.UniformGrids(cell)
     grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
     orbitals = numint.eval_ao(cell, grid_points, kpt=mf.kpt)
@@ -107,45 +118,156 @@ def test_supercell_isdf_complex():
     num_mo = mf.mo_coeff.shape[1]
     num_interp_points = 10 * num_mo
     nocc = cell.nelec[0]
-    density = np.einsum("Ri,Ri->R", orbitals_mo[:,:nocc].conj(),
-                        orbitals_mo[:,:nocc], optimize=True)
+    density = np.einsum(
+        "Ri,Ri->R", orbitals_mo[:, :nocc].conj(), orbitals_mo[:, :nocc], optimize=True
+    )
     assert np.einsum("R,R->", density, grid_inst.weights) == pytest.approx(nocc)
     with h5py.File(mf.chkfile, "r+") as fh5:
         try:
             interp_indx = fh5["interp_indx"][:]
         except KeyError:
             kmeans = KMeansCVT(grid_points, max_iteration=500)
-            interp_indx = kmeans.find_interpolating_points(num_interp_points,
-                                                           density.real)
+            interp_indx = kmeans.find_interpolating_points(
+                num_interp_points, density.real
+            )
             fh5["interp_indx"] = interp_indx
 
     from kpoint_eri.factorizations.isdf import supercell_isdf
-    chi, zeta, Theta = supercell_isdf(mf.with_df, interp_indx,
-                                      orbitals=orbitals_mo,
-                                      grid_points=grid_points)
+
+    chi, zeta, Theta = supercell_isdf(
+        mf.with_df, interp_indx, orbitals=orbitals_mo, grid_points=grid_points
+    )
     assert Theta.shape == (len(grid_points), num_interp_points)
     # Check overlap
     ovlp_ao = mf.get_ovlp()
     # should be identity matrix
-    ovlp_mo = np.einsum("pi,pq,qj->ij", mf.mo_coeff.conj(), ovlp_ao,
-                        mf.mo_coeff, optimize=True)
+    ovlp_mo = np.einsum(
+        "pi,pq,qj->ij", mf.mo_coeff.conj(), ovlp_ao, mf.mo_coeff, optimize=True
+    )
     identity = np.eye(num_mo)
     assert np.allclose(ovlp_mo, identity)
     ovlp_mu = np.einsum("Rm,R->m", Theta, grid_inst.weights, optimize=True)
     orbitals_mo_interp = orbitals_mo[interp_indx]
-    ovlp_isdf = np.einsum("mi,mj,m->ij", orbitals_mo_interp.conj(), orbitals_mo_interp,
-                          ovlp_mu, optimize=True)
+    ovlp_isdf = np.einsum(
+        "mi,mj,m->ij",
+        orbitals_mo_interp.conj(),
+        orbitals_mo_interp,
+        ovlp_mu,
+        optimize=True,
+    )
     assert np.allclose(ovlp_mo, ovlp_isdf)
     # Check ERIs.
     eri_ref = mf.with_df.ao2mo(mf.mo_coeff, kpts=mf.kpt.reshape((1, -1)))
     # Check there is a complex component
     assert np.max(np.abs(eri_ref.imag)) > 1e-3
     # for complex integrals ao2mo will yield num_mo^4 elements.
-    eri_ref = eri_ref.reshape((num_mo,)*4)
+    eri_ref = eri_ref.reshape((num_mo,) * 4)
     # THC eris
     Lijn = np.einsum("mi,mj,mn->ijn", chi.conj(), chi, zeta, optimize=True)
     eri_thc = np.einsum("ijn,nk,nl->ijkl", Lijn, chi.conj(), chi, optimize=True)
     assert np.allclose(eri_thc, eri_ref)
+
+
+# GPW evaluation of ERIs via pair densities
+def eri_from_orb_product(mf, ijR, klR, q):
+    cell = mf.cell
+    grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
+    num_grid_points = grid_points.shape[0]
+    coulG = tools.get_coulG(cell, k=q, mesh=mf.with_df.mesh)
+    weighted_coulG = coulG * cell.vol / num_grid_points
+    nmo = ijR.shape[0]
+    phase = np.exp(-1j * (np.einsum("x,Rx->R", q, grid_points)))
+    ijG = tools.fft(ijR.reshape((nmo * nmo, -1)) * phase, mf.with_df.mesh)
+    ijG *= weighted_coulG
+    vR = tools.ifft(ijG, mf.with_df.mesh).reshape((nmo, nmo, -1))
+    vR *= phase.conj()
+    eri_q = np.einsum("ijR,klR->ijkl", vR, klR, optimize=True)
+    return eri_q
+
+
+# build (ij|R) from THC factors.
+def build_isdf_orb_product(interp_orbitals, Theta, kpt_indx_ij, exp_ikr):
+    ki, kj = kpt_indx_ij[0], kpt_indx_ij[1]
+    cell_orb_product = np.einsum(
+        "Rm,im,jm->Rij",
+        Theta,
+        interp_orbitals[ki].conj(),
+        interp_orbitals[kj],
+        optimize=True,
+    )
+    bloch_orb_product = np.einsum(
+        "R,Rij->ijR", exp_ikr[ki].conj() * exp_ikr[kj], cell_orb_product, optimize=True
+    )
+    return bloch_orb_product
+
+
+# Use ISDF for |kl) and build rho_{ij}(r) using ISDF.
+def eri_from_isdf_half(
+    mf, interp_orbitals, xi_mu, q, kpts, kpts_indx_pqrs, ijR, exp_ikr
+):
+    cell = mf.cell
+    num_interp_points = interp_orbitals.shape[-1]
+    ikp, ikq, ikr, iks = kpts_indx_pqrs
+    q1 = kpts[ikq] - kpts[ikp]
+    q2 = kpts[ikr] - kpts[iks]
+    from pyscf.pbc.lib.kpts_helper import get_kconserv
+
+    kconserv = get_kconserv(cell, kpts)
+    assert xi_mu.shape[1] == num_interp_points
+    cell = mf.cell
+    grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
+    num_grid_points = grid_points.shape[0]
+    coulG = tools.get_coulG(cell, k=q, mesh=mf.with_df.mesh)
+    weighted_coulG = coulG * cell.vol / num_grid_points
+    nmo = ijR.shape[0]
+    phase = np.exp(-1j * (np.einsum("x,Rx->R", q, grid_points)))
+    ijG = tools.fft(ijR.reshape((nmo * nmo, -1)) * phase, mf.with_df.mesh)
+    ijG *= weighted_coulG
+    vR = tools.ifft(ijG, mf.with_df.mesh).reshape((nmo, nmo, -1))
+    vR *= phase.conj()
+    f_v = np.einsum(
+        "R,Rv,ijR->vij", exp_ikr[ikr].conj() * exp_ikr[iks], xi_mu, vR, optimize=True
+    )
+    # print(f_v.shape, interp_orbitals[ikr].shape)
+    eri_pqrs = np.einsum(
+        "rv,sv,vpq->pqrs",
+        interp_orbitals[ikr].conj(),
+        interp_orbitals[iks],
+        f_v,
+        optimize=True,
+    )
+    return eri_pqrs
+
+
+def eri_from_isdf(mf, interp_orbitals, xi_mu, kpts_tuple):
+    cell = mf.cell
+    num_interp_points = interp_orbitals.shape[-1]
+    assert xi_mu.shape[1] == num_interp_points
+    grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
+    num_grid_points = grid_points.shape[0]
+    kpts_indx, kpts = kpts_tuple
+    # k_q - k_p
+    q = kpts[1] - kpts[0]
+    delta_G = kpts[0] - kpts[1] + kpts[2] - kpts[3]
+    phase_factor = np.exp(-1j * (np.einsum("x,Rx->R", delta_G, grid_points)))
+    coulG = tools.get_coulG(cell, k=q, mesh=mf.with_df.mesh)
+    weighted_coulG = coulG * cell.vol / num_grid_points
+    xi_muG = tools.fft(xi_mu.T, mf.with_df.mesh)
+    xi_muG *= weighted_coulG
+    vR = tools.ifft(xi_muG, mf.with_df.mesh)
+    zeta = np.einsum("R,Rn,mR->mn", phase_factor, xi_mu, vR, optimize=True)
+    ikp, ikq, ikr, iks = kpts_indx
+    eri = np.einsum(
+        "rn,sn,mn,pm,qm->pqrs",
+        interp_orbitals[ikr].conj(),
+        interp_orbitals[iks],
+        zeta,
+        interp_orbitals[ikp].conj(),
+        interp_orbitals[ikq],
+        optimize=True,
+    )
+    return eri
+
 
 def test_kpoint_isdf():
     cell = gto.Cell()
@@ -160,21 +282,18 @@ def test_kpoint_isdf():
     3.370137329, 0.000000000, 3.370137329
     3.370137329, 3.370137329, 0.000000000"""
     cell.unit = "B"
-    cell.verbose = 4
+    cell.verbose = 0
     cell.build()
 
     kmesh = [1, 2, 3]
-    # kpts = cell.make_kpts(kmesh, scaled_center=[0.2, 0.3, 0.5])
     kpts = cell.make_kpts(kmesh)
 
-    # mf_df = scf.KRHF(cell, kpts).rs_density_fit()
-    # mf_df.with_df.mesh = cell.mesh
-    # mf_df.kernel()
     mf = scf.KRHF(cell, kpts)
     mf.chkfile = "test_isdf_supercell_kpts.chk"
     # mf.kernel()
     _, scf_dict = scf.chkfile.load_scf(mf.chkfile)
     mf.mo_coeff = scf_dict["mo_coeff"]
+    mf.with_df.max_memory = 1e9
     mf.mo_occ = scf_dict["mo_occ"]
 
     from pyscf.pbc.dft import gen_grid
@@ -182,17 +301,19 @@ def test_kpoint_isdf():
     grid_inst = gen_grid.UniformGrids(cell)
     grid_points = cell.gen_uniform_grids(mf.with_df.mesh)
     num_grid_points = grid_points.shape[0]
-    orbitals = numint.eval_ao_kpts(cell, grid_points, kpts=kpts)
-    orbitals_mo = np.einsum("kRp,kpi->kRi", orbitals, mf.mo_coeff, optimize=True)
+    bloch_orbitals_ao = np.array(numint.eval_ao_kpts(cell, grid_points, kpts=kpts))
+    bloch_orbitals_mo = np.einsum(
+        "kRp,kpi->kRi", bloch_orbitals_ao, mf.mo_coeff, optimize=True
+    )
     num_mo_per_kpt = [C.shape[-1] for C in mf.mo_coeff]
+    # Dangerous very RHF/no lin-dep dependent
     num_mo = int(np.mean(num_mo_per_kpt))
-    num_interp_points = 100 * int(np.mean(num_mo_per_kpt))
-    print(num_interp_points, num_grid_points)
+    num_interp_points = 100 * num_mo
     nocc = cell.nelec[0]
     density = np.einsum(
         "kRi,kRi->R",
-        orbitals_mo[:, :, :nocc].conj(),
-        orbitals_mo[:, :, :nocc],
+        bloch_orbitals_mo[:, :, :nocc].conj(),
+        bloch_orbitals_mo[:, :, :nocc],
         optimize=True,
     )
     num_kpts = len(kpts)
@@ -201,12 +322,15 @@ def test_kpoint_isdf():
     )
     # Cell periodic part
     # u = e^{-ik.r} phi(r)
-    exp_ikr = np.exp(-1j * np.einsum("kx,Rx->kR", kpts, grid_points))
-    cell_periodic = np.einsum("kR,kRi->kRi", exp_ikr, orbitals_mo)
+    exp_minus_ikr = np.exp(-1j * np.einsum("kx,Rx->kR", kpts, grid_points))
+    exp_ikr = exp_minus_ikr.conj()
+    cell_periodic_mo = np.einsum("kR,kRi->kRi", exp_minus_ikr, bloch_orbitals_mo)
+    cell_periodic_ao = np.einsum("kR,kRi->kRi", exp_minus_ikr, bloch_orbitals_ao)
     # sanity check
     exp_ik1r = np.exp(1j * np.einsum("x,Rx->R", kpts[1], grid_points))
-    recon_mo = np.einsum("R,Ri->Ri", exp_ik1r, cell_periodic[1])
-    assert np.allclose(recon_mo, orbitals_mo[1])
+    recon_mo = np.einsum("R,Ri->Ri", exp_ik1r, cell_periodic_mo[1])
+    assert np.allclose(recon_mo, bloch_orbitals_mo[1])
+    # Generate interpolating points.
     with h5py.File(mf.chkfile, "r+") as fh5:
         try:
             interp_indx = fh5[f"interp_indx_{num_interp_points}"][:]
@@ -221,63 +345,95 @@ def test_kpoint_isdf():
 
     # go from kRi->Rki
     # AO ISDF
-    orbitals = (
-        np.array(orbitals)
-        .transpose((1, 0, 2))
-        .reshape((num_grid_points, num_kpts * num_mo))
+    cell_periodic_ao = cell_periodic_ao.transpose((1, 0, 2)).reshape(
+        (num_grid_points, num_kpts * num_mo)
     )
-    interp_orbitals, central_tensor, Theta = kpoint_isdf(
-        mf.with_df, interp_indx, kpts, orbitals, grid_points
+    interp_orbitals_ao, Theta = kpoint_isdf(
+        mf.with_df, interp_indx, kpts, cell_periodic_ao, grid_points
     )
-    # Check overlap
+    # Test ISDF solve is reproducing orbital products
+    RHS = np.einsum(
+        "Rm,mI,mJ->RIJ",
+        Theta,
+        interp_orbitals_ao.conj(),
+        interp_orbitals_ao,
+        optimize=True,
+    )
+    LHS = np.einsum(
+        "RI,RJ->RIJ", cell_periodic_ao.conj(), cell_periodic_ao, optimize=True
+    )
+    print("delta THC: ", np.abs(np.max(LHS - RHS)))
+    LHS = LHS.reshape((num_grid_points, num_kpts, num_mo, num_kpts, num_mo))
+    # Just checking bloch <-> cell periodic transformation.
+    bloch = np.einsum("kR,pR,Rkipj->Rkipj", exp_ikr.conj(), exp_ikr, LHS, optimize=True)
+    ref = np.einsum(
+        "kRi,pRj->Rkipj", bloch_orbitals_ao.conj(), bloch_orbitals_ao, optimize=True
+    )
+    assert np.allclose(ref, bloch)
+
+    # Test overlap <u_ik | u_jk'> = delta_{kk'} S_{ikjk'}
     ovlp_ao = mf.get_ovlp()
-    exp_ikr = np.exp(1j * np.einsum("kx,Rx->kR", kpts, grid_points))
-    # ovlp_mu = np.einsum("kR,Rm,R->km", exp_ikr, Theta, grid_inst.weights)
     ovlp_mu = np.einsum("Rm,R->m", Theta, grid_inst.weights)
-    interp_orbitals = interp_orbitals.reshape((-1, num_kpts, num_mo))
+    interp_orbitals_ao = interp_orbitals_ao.reshape((-1, num_kpts, num_mo))
     ovlp_isdf = np.einsum(
         "mki,mkj,m->kij",
-        interp_orbitals.conj(),
-        interp_orbitals,
+        interp_orbitals_ao.conj(),
+        interp_orbitals_ao,
         ovlp_mu,
         optimize=True,
     )
-    orbitals = orbitals.reshape((-1, num_kpts, num_mo))
-    exp_ikr = np.exp(-1j * np.einsum("kx,Rx->kR", kpts, grid_points))
-    cell_periodic = np.einsum("kR,Rki->kRi", exp_ikr, orbitals)
+    # Sanity check for computing overlap via numerical integration.
+    cell_periodic_ao = cell_periodic_ao.reshape((-1, num_kpts, num_mo))
     ovlp_comparison = np.einsum(
-        "kRi,kRj,R->kij", cell_periodic.conj(), cell_periodic, grid_inst.weights
+        "Rki,Rkj,R->kij", cell_periodic_ao.conj(), cell_periodic_ao, grid_inst.weights
     )
     for ik in range(num_kpts):
-        print(np.max(np.abs(ovlp_ao[ik] - ovlp_isdf[ik])))
-        print(np.max(np.abs(ovlp_ao[ik] - ovlp_comparison[ik])))
+        assert np.max(np.abs(ovlp_ao[ik] - ovlp_isdf[ik])) < 1e-3
+        assert (np.max(np.abs(ovlp_ao[ik] - ovlp_comparison[ik]))) < 1e-10
+
+    from kpoint_eri.resource_estimates.utils import build_momentum_transfer_mapping
+
     momentum_map = build_momentum_transfer_mapping(cell, kpts)
-    # ao->mo transform + (mu,k,ao) -> (k, mo, mu)
-    interp_orbitals_mo = np.einsum(
-        "mkp,kpi->kim", interp_orbitals, mf.mo_coeff, optimize=True
-    )
-    iq = 2
-    k1 = 1
-    k2 = 4
-    zeta_q = central_tensor[iq]
-    k3 = momentum_map[iq][k1]
-    k4 = momentum_map[iq][k2]
+    iq = 1
+    ikp = 2
+    iks = 3
+    ikq = momentum_map[iq][ikp]
+    ikr = momentum_map[iq][iks]
     # (pk1 qk1-Q | rk2-Q s k2)
-    kpt_pqrs = [kpts[k1], kpts[k3], kpts[k4], kpts[k2]]
-    mos_pqrs = [mf.mo_coeff[k1], mf.mo_coeff[k3], mf.mo_coeff[k4], mf.mo_coeff[k2]]
+    kpt_pqrs = [kpts[ikp], kpts[ikq], kpts[ikr], kpts[iks]]
+    mos_pqrs = [mf.mo_coeff[ikp], mf.mo_coeff[ikq], mf.mo_coeff[ikr], mf.mo_coeff[iks]]
     eri_pqrs = mf.with_df.ao2mo(mos_pqrs, kpt_pqrs, compact=False).reshape(
         (num_mo,) * 4
     )
-    # u_{pk1 mu}^* u_{qk1-Q} zeta_Q_{mu,nu}
-    Lpqnu = np.einsum(
-        "pm,qm,mn->pqn", interp_orbitals_mo[k1].conj(), interp_orbitals_mo[k3], zeta_q
+    # Sanity check and rebuild ERIs using pair densities, both "exact" and from
+    # ISDF solve
+    ao_ijkl = numint.eval_ao_kpts(cell, grid_points, kpt_pqrs)
+    mo_ijkl = np.einsum("kRp,kpi->kRi", ao_ijkl, mos_pqrs)
+    # (ij|R)
+    ijR = np.einsum("Ri,Rj->ijR", mo_ijkl[0].conj(), mo_ijkl[1])
+    # (kl|R)
+    klR = np.einsum("Rk,Rl->klR", mo_ijkl[2].conj(), mo_ijkl[3])
+    # q = kpt_pqrs[2] #kpt_pqrs[1] - kpt_pqrs[0]
+    q = kpt_pqrs[1] - kpt_pqrs[0]
+    from pyscf.pbc import tools
+
+    eri_tmp = eri_from_orb_product(mf, ijR, klR, q)
+    print("delta eri: ", np.max(np.abs(eri_pqrs - eri_tmp)))
+    # Do it GPW way, build (ij|R) = phi_i*(r) phi_j(r) = e^{i(k_j-k_i).r} \sum_m Theta[r, m] u_{ki,m}^* u_{kj,m}
+    interp_orbitals_mo = np.einsum(
+        "mkp,kpi->kim", interp_orbitals_ao, mf.mo_coeff, optimize=True
     )
-    # u_{rk2-Q nu}^* u_{sk1} L_{pqnu}
-    eri_thc = np.einsum(
-        "rn,sn,pqn->pqrs", interp_orbitals_mo[k4].conj(), interp_orbitals_mo[k2], Lpqnu
+    ijR_isdf = build_isdf_orb_product(interp_orbitals_mo, Theta, [ikp, ikq], exp_ikr)
+    klR_isdf = build_isdf_orb_product(interp_orbitals_mo, Theta, [ikr, iks], exp_ikr)
+    eri_isdf = eri_from_orb_product(mf, ijR_isdf, klR_isdf, q)
+    print("delta isdf orb products: ", np.max(np.abs(eri_pqrs - eri_isdf)))
+    eri_isdf_direct = eri_from_isdf(
+        mf, interp_orbitals_mo, Theta, ([ikp, ikq, ikr, iks], kpt_pqrs)
     )
-    print(np.max(np.abs(eri_pqrs - eri_thc)))
+    print("delta isdf: ", np.max(np.abs(eri_pqrs - eri_isdf_direct)))
+
 
 if __name__ == "__main__":
-    # test_supercell_isdf_gamma()
+    test_supercell_isdf_gamma()
     test_supercell_isdf_complex()
+    test_kpoint_isdf()
