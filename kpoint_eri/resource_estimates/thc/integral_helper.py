97f1288a014dccd8d97c96fc94d21eed763692b9
Fionn Malone <fmalone@google.com>
2022-11-09 19:34:48 +0000

    Add THC integral helper.

---
+++ kpoint_eri/resource_estimates/thc/integral_helper.py
@@ -0,0 +1,125 @@
+import numpy as np
+
+from pyscf.pbc import scf
+from pyscf.pbc.lib.kpts_helper import unique, member
+
+from kpoint_eri.resource_estimates.utils.misc_utils import (
+    build_momentum_transfer_mapping,
+)
+
+from kpoint_eri.factorizations.isdf import (
+    build_G_vector_mappings_double_translation,
+    build_G_vector_mappings_single_translation,
+    build_eri_isdf_double_translation,
+    build_eri_isdf_single_translation,
+)
+
+
+class KPTHCHelperDoubleTranslation(object):
+    def __init__(
+        self,
+        chi: np.ndarray,
+        zeta: np.ndarray,
+        kmf: scf.HF,
+        nthc: int = None,
+    ):
+        """
+        Initialize a ERI object for CCSD from KP-THC factors and a
+        pyscf mean-field object
+
+        :param chi: array of interpolating orbitals of shape [num_kpts, num_mo, num_interp_points]
+        :param zeta: central tensor of dimension [num_kpts, num_G, num_G, num_interp_points, num_interp_points].
+        :param kmf: pyscf k-object.  Currently only used to obtain the number of k-points.
+                    must have an attribute kpts which len(self.kmf.kpts) returns number of
+                    kpts.
+        """
+        self.chi = chi
+        self.zeta = zeta
+        self.kmf = kmf
+        self.nk = len(self.kmf.kpts)
+        if nthc is None:
+            nthc = self.chi.shape[-1]
+            assert nthc == self.zeta[0].shape[-1]
+        self.nthc = nthc
+        self.kpts = self.kmf.kpts
+        self.k_transfer_map = build_momentum_transfer_mapping(
+            self.kmf.cell, self.kmf.kpts
+        )
+        self.reverse_k_transfer_map = np.zeros_like(
+            self.k_transfer_map
+        )  # [kidx, kmq_idx] = qidx
+        for kidx in range(self.nk):
+            for qidx in range(self.nk):
+                kmq_idx = self.k_transfer_map[qidx, kidx]
+                self.reverse_k_transfer_map[kidx, kmq_idx] = qidx
+        # Two-translation ISDF zeta[iq, dG, dG']
+        _, _, G_map_unique, delta_Gs = build_G_vector_mappings_double_translation(
+            self.kmf.cell, self.kmf.kpts, self.k_transfer_map
+        )
+        self.G_mapping = G_map_unique
+
+    def get_eri(self, ikpts):
+        """
+        Construct (pkp qkq| rkr sks) via \\sum_{mu nu} zeta[iq, dG, dG', mu, nu]
+            chi[kp,p,mu]* chi[kq,q,mu] chi[kp,p,nu]* chi[ks,s,nu]
+
+        :param ikpts: list of four integers representing the index of the kpoint in self.kmf.kpts
+        :param check_eq: optional value to confirm a symmetry in the Cholesky vectors.
+        """
+        ikp, ikq, ikr, iks = ikpts
+        q_indx = self.reverse_k_transfer_map[ikp, ikq]
+        return build_eri_isdf_double_translation(
+            self.chi, self.zeta, q_indx, ikpts, self.G_mapping
+        )
+
+
+class KPTHCHelperSingleTranslation(KPTHCHelperDoubleTranslation):
+    def __init__(
+        self,
+        chi: np.ndarray,
+        zeta: np.ndarray,
+        kmf: scf.HF,
+        nthc: int = None,
+    ):
+        """
+        Initialize a ERI object for CCSD from KP-THC factors and a
+        pyscf mean-field object
+
+        :param chi: array of interpolating orbitals of shape [num_kpts, num_mo, num_interp_points]
+        :param zeta: central tensor of dimension [num_kpts, num_G, num_G, num_interp_points, num_interp_points].
+        :param kmf: pyscf k-object.  Currently only used to obtain the number of k-points.
+                    must have an attribute kpts which len(self.kmf.kpts) returns number of
+                    kpts.
+        """
+        super().__init__(chi, zeta, kmf, nthc)
+        # one-translation ISDF zeta[iq, dG]
+        num_kpts = len(self.kmf.kpts)
+        kpts = self.kmf.kpts
+        kpts_pq = np.array(
+            [(kp, kpts[ikq]) for ikp, kp in enumerate(kpts) for ikq in range(num_kpts)]
+        )
+        kpts_pq_indx = np.array(
+            [(ikp, ikq) for ikp, kp in enumerate(kpts) for ikq in range(num_kpts)]
+        )
+        transfers = kpts_pq[:, 0] - kpts_pq[:, 1]
+        unique_q, unique_indx, unique_inverse = unique(transfers)
+        _, _, G_map_unique, delta_Gs = build_G_vector_mappings_single_translation(
+            kmf.cell, kpts, kpts_pq_indx[unique_indx]
+        )
+        self.G_mapping = G_map_unique
+        self.momentum_transfers = transfers[unique_indx]
+
+    def get_eri(self, ikpts):
+        """
+        Construct (pkp qkq| rkr sks) via \\sum_{mu nu} zeta[iq, dG, mu, nu]
+            chi[kp,p,mu]* chi[kq,q,mu] chi[kp,p,nu]* chi[ks,s,nu]
+
+        :param ikpts: list of four integers representing the index of the kpoint in self.kmf.kpts
+        :param check_eq: optional value to confirm a symmetry in the Cholesky vectors.
+        """
+        ikp, ikq, ikr, iks = ikpts
+        mom_transfer = self.kpts[ikp] - self.kpts[ikq]
+        q_indx = member(mom_transfer, self.momentum_transfers)[0]
+        return build_eri_isdf_single_translation(
+            self.chi, self.zeta, q_indx, ikpts, self.G_mapping
+        )
---
+++ kpoint_eri/resource_estimates/thc/integral_helper_test.py
@@ -0,0 +1,88 @@
+import h5py
+import numpy as np
+
+from pyscf.pbc import gto, scf, cc
+
+from kpoint_eri.factorizations.isdf import solve_kmeans_kpisdf
+from kpoint_eri.resource_estimates.thc.integral_helper import (
+        KPTHCHelperDoubleTranslation,
+        KPTHCHelperSingleTranslation
+        )
+
+def test_thc_helper():
+    cell = gto.Cell()
+    cell.atom = '''
+    C 0.000000000000   0.000000000000   0.000000000000
+    C 1.685068664391   1.685068664391   1.685068664391
+    '''
+    cell.basis = 'gth-szv'
+    cell.pseudo = 'gth-hf-rev'
+    cell.a = '''
+    0.000000000, 3.370137329, 3.370137329
+    3.370137329, 0.000000000, 3.370137329
+    3.370137329, 3.370137329, 0.000000000'''
+    cell.unit = 'B'
+    cell.verbose = 4
+    cell.build()
+
+    kmesh = [1, 1, 3]
+    kpts = cell.make_kpts(kmesh)
+    mf = scf.KRHF(cell, kpts)
+    mf.chkfile = 'integrals.chk'
+    mf.init_guess = 'chkfile'
+    mf.kernel()
+
+    exact_cc = cc.KRCCSD(mf)
+    eris = exact_cc.ao2mo()
+    exact_emp2, _, _ = exact_cc.init_amps(eris)
+
+    approx_cc = cc.KRCCSD(mf)
+    approx_cc.verbose = 0
+    nmo_k = mf.mo_coeff[0].shape[-1]
+    num_interp_points = nmo_k * 50
+    try:
+        print("Reading THC factors from file")
+        with h5py.File(mf.chkfile, "r") as fh5:
+            chi = fh5["chi"][:]
+            qs = [int(s.split("_")[1]) for s in fh5.keys() if "zeta" in s]
+            zeta = np.zeros(len(qs), dtype=object)
+            for q in qs:
+                zeta[q] = fh5[f"zeta_{q}"][:]
+    except KeyError:
+        print("Solving ISDF")
+        chi, zeta, xi, G_mapping = solve_kmeans_kpisdf(mf, num_interp_points,
+                                                       single_translation=False)
+        with h5py.File(mf.chkfile, "r+") as fh5:
+            fh5["chi"] = chi
+            for q in range(zeta.shape[0]):
+                fh5[f"zeta_{q}"] = zeta[q]
+    helper = KPTHCHelperDoubleTranslation(chi, zeta, mf)
+    from kpoint_eri.resource_estimates.cc_helper.cc_helper import build_cc
+    approx_cc = build_cc(approx_cc, helper)
+    eris = approx_cc.ao2mo(lambda x: x)
+    emp2, _, _ = approx_cc.init_amps(eris)
+    delta = abs(emp2 - exact_emp2)
+    print(" {:4d}  {:10.4e} {:10.4e} {:10.4e}".format(num_interp_points, delta, emp2, exact_emp2))
+    try:
+        print("Reading THC factors from file")
+        with h5py.File(mf.chkfile, "r") as fh5:
+            chi = fh5["chi_st"][:]
+            qs = [int(s.split("_")[1]) for s in fh5.keys() if "zeta" in s]
+            zeta = np.zeros(len(qs), dtype=object)
+            for q in qs:
+                zeta[q] = fh5[f"zeta_st_{q}"][:]
+    except KeyError:
+        print("Solving ISDF")
+        chi, zeta, xi, G_mapping = solve_kmeans_kpisdf(mf, num_interp_points,
+                                                       single_translation=True)
+        with h5py.File(mf.chkfile, "r+") as fh5:
+            fh5["chi_st"] = chi
+            for q in range(zeta.shape[0]):
+                fh5[f"zeta_st_{q}"] = zeta[q]
+    helper = KPTHCHelperSingleTranslation(chi, zeta, mf)
+    from kpoint_eri.resource_estimates.cc_helper.cc_helper import build_cc
+    approx_cc = build_cc(approx_cc, helper)
+    eris = approx_cc.ao2mo(lambda x: x)
+    emp2, _, _ = approx_cc.init_amps(eris)
+    delta = abs(emp2 - exact_emp2)
+    print(" {:4d}  {:10.4e} {:10.4e} {:10.4e}".format(num_interp_points, delta, emp2, exact_emp2))
