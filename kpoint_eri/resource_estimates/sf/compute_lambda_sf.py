fix one-body exchange sign

kpoint_eri/resource_estimates/sf/compute_lambda_sf.py
@@ -157,7 +157,7 @@ def compute_lambda_ncr2(hcore, sf_obj: NCRSingleFactorizationHelper):
             eri_kkqq_pqrs = sf_obj.get_eri([kidx, kidx, qidx, qidx])  
             h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
 
-        one_body_mat[kidx] = hcore[kidx] - 0.5 * h1_neg + h1_pos
+        one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + h1_pos
         # lambda_one_body += np.sum(np.abs(one_body_mat[kidx].real)) + np.sum(np.abs(one_body_mat[kidx].imag))
         lambda_one_body += np.sum(np.abs(one_body_mat[kidx].real) + np.abs(one_body_mat[kidx].imag))
 

kpoint_eri/resource_estimates/sparse/compute_lambda_sparse.py
@@ -106,7 +106,7 @@ def compute_lambda_ncr(hcore, sparse_int_obj: NCRSSparseFactorizationHelper):
             eri_kkqq_pqrs = sparse_int_obj.get_eri_exact([kidx, kidx, qidx, qidx])  
             h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
 
-        one_body_mat[kidx] = hcore[kidx] - 0.5 * h1_neg + h1_pos
+        one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + h1_pos
         lambda_one_body += np.sum(np.abs(one_body_mat[kidx].real)) + np.sum(np.abs(one_body_mat[kidx].imag))
     
     lambda_two_body = 0