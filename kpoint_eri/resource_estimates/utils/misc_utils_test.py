f056c464679a1db47d3ef38486f6dc59c10ca523
Fionn Malone <fmalone@google.com>
2023-03-02 22:45:55 +0000

    Add test.

---
+++ kpoint_eri/resource_estimates/utils/misc_utils_test.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+from kpoint_eri.resource_estimates.utils.misc_utils import (
+    build_momentum_transfer_mapping,
+)
+
+
+def test_momentum_transfer_map():
+    from pyscf.pbc import gto
+
+    cell = gto.Cell()
+    cell.atom = """
+    C 0.000000000000   0.000000000000   0.000000000000
+    C 1.685068664391   1.685068664391   1.685068664391
+    """
+    cell.basis = "gth-szv"
+    cell.pseudo = "gth-pade"
+    cell.a = """
+    0.000000000, 3.370137329, 3.370137329
+    3.370137329, 0.000000000, 3.370137329
+    3.370137329, 3.370137329, 0.000000000"""
+    cell.unit = "B"
+    cell.verbose = 0
+    cell.build()
+    kpts = cell.make_kpts([2, 2, 1], scaled_center=[0.1, 0.2, 0.3])
+    mom_map = build_momentum_transfer_mapping(cell, kpts)
+    for i, Q in enumerate(kpts):
+        for j, k1 in enumerate(kpts):
+            k2 = kpts[mom_map[i, j]]
+            test = Q - k1 + k2
+            assert np.amin(np.abs(test[None, :] - cell.Gv - kpts[0][None, :])) < 1e-15
