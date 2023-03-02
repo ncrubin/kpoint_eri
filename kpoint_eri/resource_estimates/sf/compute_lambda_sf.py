2e015ed10c6dd6c380dce39fa26bd7659ad8731b
Fionn Malone <fmalone@google.com>
2023-03-02 20:35:24 +0000

    Updates for sf.

--- kpoint_eri/resource_estimates/sf/compute_lambda_sf.py
+++ kpoint_eri/resource_estimates/sf/compute_lambda_sf.py
@@ -1,128 +1,19 @@
 import numpy as np
-from itertools import product
-import copy
 
-from kpoint_eri.resource_estimates import sf
-from kpoint_eri.resource_estimates.sf.ncr_integral_helper import NCRSingleFactorizationHelper
-
-def compute_lambda(
-        hcore,
-        chol,
-        kpoints,
-        momentum_map,
-        nmo_pk,
-        ):
-    nmo = sum(nmo_pk)
-    eris = np.zeros((nmo,)*4, dtype=np.complex128)
-    num_kpoints = momentum_map.shape[0]
-    offsets = np.cumsum(nmo_pk) - nmo_pk[0]
-    nchol_pk = [L.shape[-1] for L in chol]
-    lambda_W = 0.0
-    for iq in range(num_kpoints):
-        An, Bn = sf.build_ABn(chol[iq,:,:,], iq, momentum_map)
-        # sum_q sum_n (sum_{pq} |Re{A_{pq}^n}| + |Im{A_{pq}^n|)^2
-        lambda_W += np.sum(np.einsum('npq->n', np.abs(An.real)+np.abs(An.imag))**2.0)
-        lambda_W += np.sum(np.einsum('npq->n', np.abs(Bn.real)+np.abs(Bn.imag))**2.0)
-
-    # TODO check prefactor.
-    lambda_W *= 0.25
-
-    for ik in range(num_kpoints):
-        h1b = hcore[ik]
-        h2b = np.zeros_like(h1b)
-        for ik_prime in range(num_kpoints):
-            eri_pqrs = sf.build_eris_kpt(chol[0], ik, ik_prime)
-            h2b += np.einsum('pqrr->pq', eri_pqrs, optimize=True)
-        T = h1b - 0.5 * h2b
-        lambda_T = np.sum(np.abs(T.real) + np.abs(T.imag))
-
-    lambda_tot = lambda_T + lambda_W
-    return lambda_tot, lambda_T, lambda_W, sum(nchol_pk)
+from kpoint_eri.resource_estimates.sf.integral_helper_sf import (
+    SingleFactorizationHelper,
+)
 
 
-def compute_lambda_ncr(hcore, sf_obj: NCRSingleFactorizationHelper):
-    """
-    Compute one-body and two-body lambda for qubitization of 
-    single-factorized Hamiltonian.
-
-    one-body term h_pq(k) = hcore_{pq}(k) 
-                            - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
-                            + 0.5 sum_{Q}sum_{r}(pkqk|rQrQ)
-    The first term is the kinetic energy + pseudopotential (or electron-nuclear),
-    second term is from rearranging two-body operator into chemist charge-charge
-    type notation, and the third is from the one body term obtained when
-    squaring the two-body A and B operators.
-
-    two-body term V = 0.5 sum_{Q}sum_{n}(A_{n}(Q)^2 +_ B_{n}(Q)^2)
-    or V = 0.5 sum_{Q}sum_{n'}W_{n}(Q)^{2} where n' is twice the range of n.
-    lambda is 0.25sum_{Q}sum_{n'}(sum_{p,q}^{N_{k}N/2}|Re[W_{p,q}(Q)^{n}]| + |Im[W_{pq}(Q)^{n}]|)^{2}
-    note the n' sum implying W is A or B.  See note for why 0.25 in front.
-
-    :param hcore: List len(kpts) long of nmo x nmo complex hermitian arrays
-    :param sf_obj: SingleFactorization object.    
-    """
-    kpts = sf_obj.kmf.kpts
-    nkpts = len(kpts)
-    one_body_mat = np.empty((len(kpts)), dtype=object)
-    lambda_one_body = 0.
-
-    old_naux = sf_obj.naux # need to reset naux for one-body computation
-    sf_obj.naux = sf_obj.chol[0, 0].shape[0]
-
-    for kidx in range(len(kpts)):
-        # matrices for - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
-        # and  + 0.5 sum_{Q}sum_{r}(pkqk|rQrQ)
-        h1_pos = np.zeros_like(hcore[kidx])
-        h1_neg = np.zeros_like(hcore[kidx])
-        for qidx in range(len(kpts)):
-            # - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
-            eri_kqqk_pqrs = sf_obj.get_eri([kidx, qidx, qidx, kidx]) 
-            h1_neg -= np.einsum('prrq->pq', eri_kqqk_pqrs, optimize=True) / nkpts
-            # + 0.5 sum_{Q}sum_{r}(pkqk|rQrQ)
-            eri_kkqq_pqrs = sf_obj.get_eri([kidx, kidx, qidx, qidx])  
-            h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
-
-        one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + h1_pos
-        one_eigs, _ = np.linalg.eigh(one_body_mat[kidx])
-        lambda_one_body += np.sum(np.abs(one_eigs))
-
-    ##############################################################################
-    #
-    # \lambda_{V} = \frac 12 \sum_{\Q}\sum_{n}^{M}\left
-    # (\sum_{\K,pq}(|\Rea[L_{p\K,q\K-\Q,n}]|+|\Ima[L_{p\K,q\K-\Q,n}]|)\right)^{2} 
-    #
-    # chol = [nkpts, nkpts, naux, nao, nao] 
-    #
-    ##############################################################################
-    sf_obj.naux = old_naux  # reset naux to original value
-    nmo = sf_obj.nao
-    lambda_two_body = 0
-    for qidx in range(len(kpts)):
-        # A and B are W
-        # A, B = sf_obj.build_AB_from_chol(qidx) # [naux, nao * nk, nao * nk]
-        # A /= np.sqrt(nkpts)
-        # B /= np.sqrt(nkpts)
-        for nn in range(sf_obj.naux):
-            sum_to_be_squared = 0
-            for kidx in range(sf_obj.nk):
-                k_minus_q_idx = sf_obj.k_transfer_map[qidx, kidx]
-                sum_to_be_squared += np.sum(np.abs(sf_obj.chol[kidx, k_minus_q_idx][nn, :, :].real / np.sqrt(nkpts)))
-                sum_to_be_squared += np.sum(np.abs(sf_obj.chol[kidx, k_minus_q_idx][nn, :, :].imag / np.sqrt(nkpts)))
-            lambda_two_body += sum_to_be_squared**2
-    lambda_two_body *= 0.5
-
-    lambda_tot = lambda_one_body + lambda_two_body
-    return lambda_tot, lambda_one_body, lambda_two_body
-
-def compute_lambda_ncr2(hcore, sf_obj: NCRSingleFactorizationHelper):
+def compute_lambda(hcore: np.ndarray, sf_obj: SingleFactorizationHelper):
     """
     this should be the same as above but written in a more explicit way
 
-    Compute one-body and two-body lambda for qubitization of 
+    Compute one-body and two-body lambda for qubitization of
     single-factorized Hamiltonian.
 
-    one-body term h_pq(k) = hcore_{pq}(k) 
-                            - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
+    one-body term h_pq(k) = hcore_{pq}(k)
+                            - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk)
                             + sum_{Q}sum_{r}(pkqk|rQrQ)
     The first term is the kinetic energy + pseudopotential (or electron-nuclear),
     second term is from rearranging two-body operator into chemist charge-charge
@@ -134,53 +25,58 @@ def compute_lambda_ncr2(hcore, sf_obj: NCRSingleFactorizationHelper):
     lambda is 0.5sum_{Q}sum_{n'}(sum_{p,q}^{N_{k}N/2}|Re[W_{p,q}(Q)^{n}]| + |Im[W_{pq}(Q)^{n}]|)^{2}
 
     :param hcore: List len(kpts) long of nmo x nmo complex hermitian arrays
-    :param sf_obj: SingleFactorization object.    
+    :param sf_obj: SingleFactorization object.
     """
     kpts = sf_obj.kmf.kpts
     nkpts = len(kpts)
     one_body_mat = np.empty((len(kpts)), dtype=object)
-    lambda_one_body = 0.
+    lambda_one_body = 0.0
 
-    old_naux = sf_obj.naux # need to reset naux for one-body computation
+    old_naux = sf_obj.naux  # need to reset naux for one-body computation
     sf_obj.naux = sf_obj.chol[0, 0].shape[0]
 
     for kidx in range(len(kpts)):
-        # matrices for - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
+        # matrices for - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk)
         # and  + 0.5 sum_{Q}sum_{r}(pkqk|rQrQ)
         h1_pos = np.zeros_like(hcore[kidx])
         h1_neg = np.zeros_like(hcore[kidx])
         for qidx in range(len(kpts)):
-            # - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk) 
-            eri_kqqk_pqrs = sf_obj.get_eri([kidx, qidx, qidx, kidx]) 
-            h1_neg -= np.einsum('prrq->pq', eri_kqqk_pqrs, optimize=True) / nkpts
+            # - 0.5 * sum_{Q}sum_{r}(pkrQ|rQqk)
+            eri_kqqk_pqrs = sf_obj.get_eri([kidx, qidx, qidx, kidx])
+            h1_neg -= np.einsum("prrq->pq", eri_kqqk_pqrs, optimize=True) / nkpts
             # + sum_{Q}sum_{r}(pkqk|rQrQ)
-            eri_kkqq_pqrs = sf_obj.get_eri([kidx, kidx, qidx, qidx])  
-            h1_pos += np.einsum('pqrr->pq', eri_kkqq_pqrs) / nkpts
+            eri_kkqq_pqrs = sf_obj.get_eri([kidx, kidx, qidx, qidx])
+            h1_pos += np.einsum("pqrr->pq", eri_kkqq_pqrs) / nkpts
 
         one_body_mat[kidx] = hcore[kidx] + 0.5 * h1_neg + h1_pos
         # lambda_one_body += np.sum(np.abs(one_body_mat[kidx].real)) + np.sum(np.abs(one_body_mat[kidx].imag))
-        lambda_one_body += np.sum(np.abs(one_body_mat[kidx].real) + np.abs(one_body_mat[kidx].imag))
+        lambda_one_body += np.sum(
+            np.abs(one_body_mat[kidx].real) + np.abs(one_body_mat[kidx].imag)
+        )
 
- 
     ##############################################################################
     #
     # \lambda_{V} = \frac 12 \sum_{\Q}\sum_{n}^{M}\left
-    # (\sum_{\K,pq}(|\Rea[L_{p\K,q\K-\Q,n}]|+|\Ima[L_{p\K,q\K-\Q,n}]|)\right)^{2} 
+    # (\sum_{\K,pq}(|\Rea[L_{p\K,q\K-\Q,n}]|+|\Ima[L_{p\K,q\K-\Q,n}]|)\right)^{2}
     #
-    # chol = [nkpts, nkpts, naux, nao, nao] 
+    # chol = [nkpts, nkpts, naux, nao, nao]
     #
     ##############################################################################
     sf_obj.naux = old_naux  # reset naux to original value
-    # this part needs to change 
+    # this part needs to change
     lambda_two_body = 0
     for qidx in range(len(kpts)):
         # A and B are W
-        A, B = sf_obj.build_AB_from_chol(qidx) # [naux, nao * nk, nao * nk]
+        A, B = sf_obj.build_AB_from_chol(qidx)  # [naux, nao * nk, nao * nk]
         A /= np.sqrt(nkpts)
         B /= np.sqrt(nkpts)
         # sum_q sum_n (sum_{pq} |Re{A_{pq}^n}| + |Im{A_{pq}^n|)^2
-        lambda_two_body += np.sum(np.einsum('npq->n', np.abs(A.real) + np.abs(A.imag))**2)
-        lambda_two_body += np.sum(np.einsum('npq->n', np.abs(B.real) + np.abs(B.imag))**2)
+        lambda_two_body += np.sum(
+            np.einsum("npq->n", np.abs(A.real) + np.abs(A.imag)) ** 2
+        )
+        lambda_two_body += np.sum(
+            np.einsum("npq->n", np.abs(B.real) + np.abs(B.imag)) ** 2
+        )
         del A
         del B
         # lambda_two_body += np.sum(np.einsum('npq->n', np.abs(A)**2))
--- kpoint_eri/resource_estimates/sf/compute_sf_resources.py
+++ kpoint_eri/resource_estimates/sf/compute_sf_resources.py
@@ -10,12 +10,14 @@ import numpy as np
 from numpy.lib.scimath import arccos, arcsin  # has analytic continutn to cplx
 from sympy import factorint
 
-from openfermion.resource_estimates.utils import (QR, QI, power_two,)
+from openfermion.resource_estimates.utils import (
+    QI,
+)
 from openfermion.resource_estimates.utils import QR2 as QR2_of
 
 
 def QR_ncr(L, M1):
-    """
+    r"""
     QR[Ll_, m_] := Ceiling[MinValue[{Ll/2^k + m*(2^k - 1), k >= 0}, k \[Element] Integers]];
     """
     k = 0.5 * np.log2(L / M1)
@@ -37,20 +39,23 @@ def QR_ncr(L, M1):
 
 def QR2(L1, L2, M):
     """
-    Table[Ceiling[L1/2^k1]*Ceiling[L2/2^k2] + M*(2^(k1 + k2) - 1), {k1, 1,
-   10}, {k2, 1, 10}]
+     Table[Ceiling[L1/2^k1]*Ceiling[L2/2^k2] + M*(2^(k1 + k2) - 1), {k1, 1,
+    10}, {k2, 1, 10}]
     """
     min_val = np.inf
     for k1 in range(1, 11):
         for k2 in range(1, 11):
-            test_val = np.ceil(L1 / (2**k1)) * np.ceil(L2/ (2**k2)) + M * (2 ** (k1 + k2) - 1)
-            if test_val  < min_val:
+            test_val = np.ceil(L1 / (2**k1)) * np.ceil(L2 / (2**k2)) + M * (
+                2 ** (k1 + k2) - 1
+            )
+            if test_val < min_val:
                 min_val = test_val
     return int(min_val)
 
+
 def QI2(L1, Lv2):
     """
-    QI2[L1_, L2_] := 
+    QI2[L1_, L2_] :=
     Min[Table[
     Ceiling[L1/2^k1]*Ceiling[L2/2^k2] + 2^(k1 + k2), {k1, 1, 10}, {k2,
       1, 10}]];
@@ -58,26 +63,30 @@ def QI2(L1, Lv2):
     min_val = np.inf
     for k1 in range(1, 11):
         for k2 in range(1, 11):
-            test_val = np.ceil(L1 / (2**k1)) * np.ceil(Lv2 / (2**k2)) + 2**(k1 + k2)
+            test_val = np.ceil(L1 / (2**k1)) * np.ceil(Lv2 / (2**k2)) + 2 ** (
+                k1 + k2
+            )
             if test_val < min_val:
                 min_val = test_val
     return int(min_val)
 
 
-def kpoint_single_factorization_costs(n: int, 
-                                      lam: float, 
-                                      M: int, 
-                                      dE: float, 
-                                      chi: int, 
-                                      stps: int, 
-                                      Nkx: int,
-                                      Nky: int,
-                                      Nkz: int,
-                                      verbose: bool=False) -> Tuple[int, int, int]:
-    """ Determine fault-tolerant costs using SF decomposition in quantum chem
+def kpoint_single_factorization_costs(
+    n: int,
+    lam: float,
+    M: int,
+    dE: float,
+    chi: int,
+    stps: int,
+    Nkx: int,
+    Nky: int,
+    Nkz: int,
+    verbose: bool = False,
+) -> Tuple[int, int, int]:
+    """Determine fault-tolerant costs using SF decomposition in quantum chem
 
     Args:
-        n (int) - the number of spin-orbitals. When using this for 
+        n (int) - the number of spin-orbitals. When using this for
                   k-point sampling, this is equivalent to N times N_k.
         lam (float) - the lambda-value for the Hamiltonian
         M (int) - The combined number of values of n
@@ -123,85 +132,122 @@ def kpoint_single_factorization_costs(n: int,
     for p in range(20):
         # JJG note: arccos arg may be > 1
         # v = Round[2^p/(2*\[Pi])*ArcCos[2^nL/Sqrt[(L + 1)/2^\[Eta]]/2]];
-        v = np.round(np.power(2,p+1) / (2 * np.pi) * arccos(np.power(2,nMN) / np.sqrt(M * Nk + 1) / 2))
+        v = np.round(
+            np.power(2, p + 1)
+            / (2 * np.pi)
+            * arccos(np.power(2, nMN) / np.sqrt(M * Nk + 1) / 2)
+        )
         #   oh[[p]] = stps*(1/N[Sin[3*ArcSin[Cos[v*2*\[Pi]/2^p]*Sqrt[(L + 1)/2^\[Eta]]/2^nL]]^2] - 1) + 4*p];
         # stps*(1/N[Sin[3*ArcSin[Cos[v*2*\[Pi]/2^p]*Sqrt[M*Nk + 1]/2^nMN]]^2] - 1) + 4*p]
-        oh[p] = np.real(stps * (1 / (np.sin(3 * arcsin(np.cos(v * 2 * np.pi / np.power(2,p+1)) * \
-                                                       np.sqrt(M * Nk + 1) / np.power(2,nMN)
-                                                       )
-                                            )**2
-                                    ) - 1
-                                ) + 4 * (p + 1)
+        oh[p] = np.real(
+            stps
+            * (
+                1
+                / (
+                    np.sin(
+                        3
+                        * arcsin(
+                            np.cos(v * 2 * np.pi / np.power(2, p + 1))
+                            * np.sqrt(M * Nk + 1)
+                            / np.power(2, nMN)
                         )
+                    )
+                    ** 2
+                )
+                - 1
+            )
+            + 4 * (p + 1)
+        )
 
     # Bits of precision for rotation
-    br1 = int(np.argmin(oh) + 1) 
+    br1 = int(np.argmin(oh) + 1)
 
     oh2 = [0] * 20
     for p in range(20):
         # JJG note: arccos arg may be > 1
         # v = Round[2^p/(2*\[Pi])*ArcCos[2^nL/Sqrt[(L + 1)/2^\[Eta]]/2]];
-        v = np.round(np.power(2,p+1) / (2 * np.pi) * arccos(np.power(2,nL) / np.sqrt(L / np.power(2, eta)) / 2))
+        v = np.round(
+            np.power(2, p + 1)
+            / (2 * np.pi)
+            * arccos(np.power(2, nL) / np.sqrt(L / np.power(2, eta)) / 2)
+        )
         #   oh[[p]] = stps*(1/N[Sin[3*ArcSin[Cos[v*2*\[Pi]/2^p]*Sqrt[(L + 1)/2^\[Eta]]/2^nL]]^2] - 1) + 4*p];
         # oh2[[p]] = stps*(1/N[Sin[3*ArcSin[Cos[v*2*\[Pi]/2^p]*Sqrt[L/2^\[Eta]]/2^nL]]^2] - 1) + 4*p];
-        oh2[p] = np.real(stps * (1 / (np.sin(3 * arcsin(np.cos(v * 2 * np.pi / np.power(2,p+1)) * \
-                                                       np.sqrt(L / np.power(2, eta)) / np.power(2,nL)
-                                                       )
-                                            )**2
-                                    ) - 1
-                                ) + 4 * (p + 1)
+        oh2[p] = np.real(
+            stps
+            * (
+                1
+                / (
+                    np.sin(
+                        3
+                        * arcsin(
+                            np.cos(v * 2 * np.pi / np.power(2, p + 1))
+                            * np.sqrt(L / np.power(2, eta))
+                            / np.power(2, nL)
                         )
+                    )
+                    ** 2
+                )
+                - 1
+            )
+            + 4 * (p + 1)
+        )
     br2 = int(np.argmin(oh2) + 1)
 
     # Cost of preparing the equal superposition state on the 1st register in 1a
-    cost1a = 2*(3*nMN + 2*br1 - 9)
+    cost1a = 2 * (3 * nMN + 2 * br1 - 9)
 
     # We have added two qubits for ell <> 0.
     # bMN = 2*(np.ceil(np.log2(Nk)) + np.ceil(np.log2(M))) + chi + 2
-    bMN = nMN + 2*np.ceil(np.log2(Nk)) + chi + 2
+    bMN = nMN + 2 * np.ceil(np.log2(Nk)) + chi + 2
 
     # QROM costs for first register preparation in step 1(b)
     cost1b = QR_ncr(M * Nk + 1, bMN)[-1] + QI(M * Nk + 1)[-1]
 
     # The inequality test of cost chi in step 1(c) and the controlled swap with
     # cost nL + 1 in step 1(d) (and their inversions).
-    cost1cd = 2*(chi + np.ceil(np.log2((Nk)) + np.ceil(np.log2(M)) + 1))
+    cost1cd = 2 * (chi + np.ceil(np.log2((Nk)) + np.ceil(np.log2(M)) + 1))
 
     # Cost of preparing equal superposition of p and q registers in step 2(a).
-    cost2a = 4*(3*nL - 3*eta + 2*br2 - 9)
+    cost2a = 4 * (3 * nL - 3 * eta + 2 * br2 - 9)
 
     # The output size for the QROM for the second state preparation.
-    bp = 6*np.ceil(np.log2(Nk)/3) + 4*nN + chi + 3
+    bp = 6 * np.ceil(np.log2(Nk) / 3) + 4 * nN + chi + 3
 
     # Cost of QROMs for state preparation on p and q in step 2 (c).
-    cost2b = QR2(M * Nk + 1, L, bp) + QI2(M * Nk + 1, L) + QR2(M * Nk, L, bp) + QI2(M * Nk, L)
+    cost2b = (
+        QR2(M * Nk + 1, L, bp)
+        + QI2(M * Nk + 1, L)
+        + QR2(M * Nk, L, bp)
+        + QI2(M * Nk, L)
+    )
 
-    # The cost of the inequality test and controlled swap for the 
+    # The cost of the inequality test and controlled swap for the
     # quantum alias sampling in steps 2(c) and (d).
-    cost2cd =  4*(chi + 3*np.ceil(np.log2(Nk)/3) + 2*nN + 1)
+    cost2cd = 4 * (chi + 3 * np.ceil(np.log2(Nk) / 3) + 2 * nN + 1)
 
     # Computing k - Q and k' - Q, then below we perform adjustments for non - modular arithmetic.
     cost3 = 4 * nNk
-    if Nkx == 2**np.ceil(np.log2(Nkx)):
+    if Nkx == 2 ** np.ceil(np.log2(Nkx)):
         cost3 = cost3 - 2 * np.ceil(np.log2(Nkx))
-    if Nky == 2**np.ceil(np.log2(Nky)):
+    if Nky == 2 ** np.ceil(np.log2(Nky)):
         cost3 = cost3 - 2 * np.ceil(np.log2(Nky))
-    if Nkz == 2**np.ceil(np.log2(Nkz)):
+    if Nkz == 2 ** np.ceil(np.log2(Nkz)):
         cost3 = cost3 - 2 * np.ceil(np.log2(Nkz))
 
     # The SELECT operation in step 4, which needs to be done twice.
-    cost4 = 2*(3*n*Nk - 4)
+    cost4 = 2 * (3 * n * Nk - 4)
 
     # The reflection used on the 2nd register in the middle, with cost in step 6
     cost6 = nL + chi + 5
-    
+
     # Step 7 involves performing steps 2 to 5 again, which are accounted for
     # above, but there is one extra Toffoli for checking that l <> 0.
     cost7 = 4
 
     # The total reflection for the quantum walk in step 9.
     # with an extra + 1 for k - point sampling.
-    cost9 = nMN + nL + 2*chi + 4
+    cost9 = nMN + nL + 2 * chi + 4
 
     # The two Toffolis for the control for phase estimation and iteration,
     # given in step 10.
@@ -211,7 +257,20 @@ def kpoint_single_factorization_costs(n: int,
     iters = np.ceil(np.pi * lam / (dE * 2))
 
     # The total Toffoli costs for a step.
-    cost = cost1a + cost1b + cost1cd + cost2a + cost2b + cost2cd + cost3 + cost4 + cost6 + cost7 + cost9 + cost10
+    cost = (
+        cost1a
+        + cost1b
+        + cost1cd
+        + cost2a
+        + cost2b
+        + cost2cd
+        + cost3
+        + cost4
+        + cost6
+        + cost7
+        + cost9
+        + cost10
+    )
 
     # Control for phase estimation and its iteration
     ac1 = 2 * np.ceil(np.log2(iters)) - 1
@@ -230,7 +289,7 @@ def kpoint_single_factorization_costs(n: int,
 
     kp = QR2_of(M * Nk + 1, L, bp)[:2]
 
-    ac6 = chi 
+    ac6 = chi
 
     # The equal superposition state for the second state preparation
     ac7 = np.max([br1, br2])
@@ -239,8 +298,11 @@ def kpoint_single_factorization_costs(n: int,
     ac8 = 4
 
     # The QROM on the p & q registers
-    ac9 = kp[0] * kp[1] * bp + np.ceil(np.log2(
-        (M * Nk + 1) / kp[0])) + np.ceil(np.log2(L / kp[1]))
+    ac9 = (
+        kp[0] * kp[1] * bp
+        + np.ceil(np.log2((M * Nk + 1) / kp[0]))
+        + np.ceil(np.log2(L / kp[1]))
+    )
 
     if verbose:
         print("[*] Top of routine")
@@ -262,14 +324,13 @@ def kpoint_single_factorization_costs(n: int,
     return step_cost, total_cost, total_qubit_count
 
 
-
 if __name__ == "__main__":
     n = 152
     lam = 3071.8
     L = 275
     dE = 0.001
     chi = 10
-    
+
     # stps = kpoint_single_factorization_costs(n, lam, L, 8, dE, chi, 20_000)[0]
     res = kpoint_single_factorization_costs(n, lam, L, dE, chi, 20_000, 3, 3, 3)
     # 1663687, 8027577592851, 438447}
